2aa7498f0d5c93409fcc94135e8ff8b201dc7622
AzizbekDev <36416819+AzizbekDev@users.noreply.github.com>
2021-12-09 17:18:27 +0500

    dashboard widgets commented

--- config/voyager.php
+++ config/voyager.php
@@ -144,9 +144,9 @@ return [
         ],
 
         'widgets' => [
-            'TCG\\Voyager\\Widgets\\UserDimmer',
-            'TCG\\Voyager\\Widgets\\PostDimmer',
-            'TCG\\Voyager\\Widgets\\PageDimmer',
+            // 'TCG\\Voyager\\Widgets\\UserDimmer',
+            // 'TCG\\Voyager\\Widgets\\PostDimmer',
+            // 'TCG\\Voyager\\Widgets\\PageDimmer',
         ],
 
     ],
