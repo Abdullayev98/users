b02fa84018014e63aa0dae07e6892812e3559f47
Abram <abram4ik73@gmail.com>
2021-12-09 18:14:07 +0500

    added locale uz

---
+++ resources/lang/uz/login.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'loggingin'    => 'Tizimga kirish',
+    'signin_below' => 'Boshqaruv paneliga kirish',
+    'welcome'      => 'Панель управления, которой не хватало в Laravel',
+];
