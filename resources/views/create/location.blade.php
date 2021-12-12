2fba809b17deb16118721685b369d8429b7d373f
Farrux <farruxzokirov230596@gmail.com>
2021-12-12 18:44:30 +0500

    location.blade.php

--- resources/views/create/location.blade.php
+++ resources/views/create/location.blade.php
@@ -39,7 +39,7 @@
                   <svg class="h-4 w-4 text-purple-500"  width="12" height="12" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">  <path stroke="none" d="M0 0h24v24H0z"/>  <path d="M21 3L14.5 21a.55 .55 0 0 1 -1 0L10 14L3 10.5a.55 .55 0 0 1 0 -1L21 3" /></svg>
                 </button>
             </div>
-            <div id="addinput">
+            <div id="addinput" class="flex gap-y-2 flex-col">
               
   
             </div> 
@@ -55,7 +55,7 @@
               </svg>
               <span >Добавить ещё адрес</span>
              </button>
-             <div id="map"></div>
+             <div id="map" style="height: 400px;"></div>
           </div>
         </div>
       </div>
@@ -68,81 +68,59 @@
 </div>
 <!-- </form> -->
 
-<div class="flex items-center gap-x-2">
-  <div class="flex items-center rounded-lg border  w-full py-1">
-    <button class="flex-shrink-0 border-transparent text-teal-500 text-md py-1 px-2 rounded focus:outline-none" type="button">
-      A
-    </button>
-    <input class="appearance-none bg-transparent w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none" type="text" placeholder="Город, Улица, Дом" aria-label="Full name">
-    <button class="flex-shrink-0 border-transparent border-4 text-teal-500 hover:text-teal-800 text-sm py-1 px-2 rounded" type="button">
-      <svg class="h-4 w-4 text-purple-500"  width="12" height="12" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">  <path stroke="none" d="M0 0h24v24H0z"/>  <path d="M21 3L14.5 21a.55 .55 0 0 1 -1 0L10 14L3 10.5a.55 .55 0 0 1 0 -1L21 3" /></svg>
-    </button>
-  </div>
 
-  <button class="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow">
-    <svg xmlns="http://www.w3.org/2000/svg" width="18" height="20" fill="none"><path fill-rule="evenodd" clip-rule="evenodd" d="M3.25 2.95v-.2A2.75 2.75 0 0 1 6 0h6a2.75 2.75 0 0 1 2.75 2.75v.2h2.45a.8.8 0 0 1 0 1.6H.8a.8.8 0 1 1 0-1.6h2.45zm10 .05v-.25c0-.69-.56-1.25-1.25-1.25H6c-.69 0-1.25.56-1.25 1.25V3h8.5z" fill="#666"/><path d="M14.704 6.72a.8.8 0 1 1 1.592.16l-.996 9.915a2.799 2.799 0 0 1-2.8 2.802h-7c-1.55 0-2.8-1.252-2.796-2.723l-1-9.994a.8.8 0 1 1 1.592-.16L4.3 16.794c0 .668.534 1.203 1.2 1.203h7c.665 0 1.2-.536 1.204-1.282l1-9.995z" fill="#666"/><path d="M12.344 7.178a.75.75 0 1 0-1.494-.13l-.784 8.965a.75.75 0 0 0 1.494.13l.784-8.965zm-6.779 0a.75.75 0 0 1 1.495-.13l.784 8.965a.75.75 0 0 1-1.494.13l-.785-8.965z" fill="#666"/></svg>
-  </button>
+@endsection
+
+@section("javasript")
+
+    <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js" defer></script>
 
 
-</div> 
-<script src="{{asset('vendor/jquery/jquery.min.js')}}"></script>
-
+    
 <script>
   var x = 1;
-var field ='<div><input type="text" name="field_name[]" value="" /><a href="javascript:void(0);" class="remove_button" title="Add field"><button class=" btn btn-danger" style=" margin:5px;">Remove This</button></a></div>'
-$(".addbtn").click(function(){
+var field ='<div class="flex items-center gap-x-2"><div class="flex items-center rounded-lg border  w-full py-1"> <button class="flex-shrink-0 border-transparent text-teal-500 text-md py-1 px-2 rounded focus:outline-none" type="button">  A </button> <input class="appearance-none bg-transparent w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none" type="text" placeholder="Город, Улица, Дом" aria-label="Full name">  <button class="flex-shrink-0 border-transparent border-4 text-teal-500 hover:text-teal-800 text-sm py-1 px-2 rounded" type="button">   <svg class="h-4 w-4 text-purple-500"  width="12" height="12" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">  <path stroke="none" d="M0 0h24v24H0z"/>  <path d="M21 3L14.5 21a.55 .55 0 0 1 -1 0L10 14L3 10.5a.55 .55 0 0 1 0 -1L21 3" /></svg>  </button> </div><button id="remove_inputs" class="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow">  <svg xmlns="http://www.w3.org/2000/svg" width="18" height="20" fill="none"><path fill-rule="evenodd" clip-rule="evenodd" d="M3.25 2.95v-.2A2.75 2.75 0 0 1 6 0h6a2.75 2.75 0 0 1 2.75 2.75v.2h2.45a.8.8 0 0 1 0 1.6H.8a.8.8 0 1 1 0-1.6h2.45zm10 .05v-.25c0-.69-.56-1.25-1.25-1.25H6c-.69 0-1.25.56-1.25 1.25V3h8.5z" fill="#666"/><path d="M14.704 6.72a.8.8 0 1 1 1.592.16l-.996 9.915a2.799 2.799 0 0 1-2.8 2.802h-7c-1.55 0-2.8-1.252-2.796-2.723l-1-9.994a.8.8 0 1 1 1.592-.16L4.3 16.794c0 .668.534 1.203 1.2 1.203h7c.665 0 1.2-.536 1.204-1.282l1-9.995z" fill="#666"/><path d="M12.344 7.178a.75.75 0 1 0-1.494-.13l-.784 8.965a.75.75 0 0 0 1.494.13l.784-8.965zm-6.779 0a.75.75 0 0 1 1.495-.13l.784 8.965a.75.75 0 0 1-1.494.13l-.785-8.965z" fill="#666"/></svg> </button></div> ';
+$("#addbtn").click(function(){
     if(x < 10){
-        $(".addinput").append(field);
+        $("#addinput").append(field);
         x++;
     }else{
         alert("max ten field allowed");
     }
 });
-$(".addinput").on("click" ,".remove_button" , function(){
+$("#addinput").on("click" ,"#remove_inputs" , function(){
     $(this).parent("div").remove();
         x--;
 });
 </script>
-<script>
-  var counter = 1; //limits amount of transactions
-function addElements() {
-    if (counter < 5) //only allows 4 additional transactions
-    {
 
-      
-        // let div = document.createElement('div');
-        // div.className = 'flex items-center gap-x-2';
-        // document.getElementById('addinput').appendChild(div);
+<script src="https://api-maps.yandex.ru/2.1/?apikey=f4b34baa-cbd1-432b-865b-9562afa3fcdb&lang=ru_RU" type="text/javascript">
+</script>
+<script type="text/javascript">
+  ymaps.ready(init);
 
-        // let div2 = document.createElement('div');
-        // div2.className = 'flex items-center rounded-lg border  w-full py-1';
-        // div.appendChild(div2);
+function init () {
+    var myMap = new ymaps.Map('map', {
+            center: [41.068725, 69.027005],
+            zoom: 16
+        }),
 
-        // let btn1 = document.createElement('button');
-        // btn1.className = "flex-shrink-0 border-transparent text-teal-500 text-md py-1 px-2 rounded focus:outline-none";
-        // btn1.innerText = 'B';
-        // div2.appendChild(btn1);
+        myPlacemark1 = new ymaps.Placemark([41.068725, 69.027005], {
+            balloonContent: 'Маленькая иконка'
+        }, {
+            iconLayout: 'default#image',
+            iconImageClipRect: [[0,0], [0, 0]],
+            iconImageHref: "http://labs.google.com/ridefinder/images/mm_20_red.png",
+            iconImageSize: [12, 15],
+            iconImageOffset: [0, -0],
+        }
+
+        );
+
+    myMap.geoObjects.add(myPlacemark1)
         
-        // let input1 = document.createElement('input');
-        // input1.className ="appearance-none bg-transparent w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none";
-        // input1.type = 'text';
-        // input1.placeholder = "Город, Улица, Дом";
-        // div2.appendChild(input1);
-
-        // let btn2 = document.createElement('button');
-        // btn2.className = "flex-shrink-0 border-transparent border-4 text-teal-500 hover:text-teal-800 text-sm py-1 px-2 rounded";
-        // div2.appendChild(btn2);
-
-        $('addinput').append('<div class="flex items-center gap-x-2">  <div class="flex items-center rounded-lg border  w-full py-1">    <button class="flex-shrink-0 border-transparent text-teal-500 text-md py-1 px-2 rounded focus:outline-none" type="button">      A    </button>    <input class="appearance-none bg-transparent w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none" type="text" placeholder="Город, Улица, Дом" aria-label="Full name">    <button class="flex-shrink-0 border-transparent border-4 text-teal-500 hover:text-teal-800 text-sm py-1 px-2 rounded" type="button">      <svg class="h-4 w-4 text-purple-500"  width="12" height="12" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">  <path stroke="none" d="M0 0h24v24H0z"/>  <path d="M21 3L14.5 21a.55 .55 0 0 1 -1 0L10 14L3 10.5a.55 .55 0 0 1 0 -1L21 3" /></svg>    </button>  </div>  <button class="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow">    <svg xmlns="http://www.w3.org/2000/svg" width="18" height="20" fill="none"><path fill-rule="evenodd" clip-rule="evenodd" d="M3.25 2.95v-.2A2.75 2.75 0 0 1 6 0h6a2.75 2.75 0 0 1 2.75 2.75v.2h2.45a.8.8 0 0 1 0 1.6H.8a.8.8 0 1 1 0-1.6h2.45zm10 .05v-.25c0-.69-.56-1.25-1.25-1.25H6c-.69 0-1.25.56-1.25 1.25V3h8.5z" fill="#666"/><path d="M14.704 6.72a.8.8 0 1 1 1.592.16l-.996 9.915a2.799 2.799 0 0 1-2.8 2.802h-7c-1.55 0-2.8-1.252-2.796-2.723l-1-9.994a.8.8 0 1 1 1.592-.16L4.3 16.794c0 .668.534 1.203 1.2 1.203h7c.665 0 1.2-.536 1.204-1.282l1-9.995z" fill="#666"/><path d="M12.344 7.178a.75.75 0 1 0-1.494-.13l-.784 8.965a.75.75 0 0 0 1.494.13l.784-8.965zm-6.779 0a.75.75 0 0 1 1.495-.13l.784 8.965a.75.75 0 0 1-1.494.13l-.785-8.965z" fill="#666"/></svg>  </button></div> ');
-
-       
-    }
-
-    counter++;
-    if (counter >= 6) {
-        alert("You have reached the maximum transactions.");
-    }
 }
+
 </script>
 
 @endsection
