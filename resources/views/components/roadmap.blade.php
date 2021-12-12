576f69ef6b8e1eb4c766c3e35d18b9b7aa28111a
Avazbek Nematov <68123420+avacoderuz@users.noreply.github.com>
2021-12-12 15:40:02 +0500

    Components

---
+++ resources/views/components/roadmap.blade.php
@@ -0,0 +1,33 @@
+<div class="bg-[#f5f5fa] bg-center bg-cover mt-4 h-40 py-4 invisible lg:visible">
+    <div class="row-start-2 row-span-2 mx-56 h-full">
+        <div class="grid grid-cols-3 gap-6 h-full">
+            <div class="text-left h-full">
+                <div class="grid grid-cols-3 h-full">
+                    <div class="step w-full text-center h-full text-8xl pt-4 text-[#a199aa]">1</div>
+                    <div class="w-full col-span-2">
+                        <div class="text-left text-xl font-bold pt-4 w-full h-1/2">Опишите задание</div>
+                        <div class="text-left text-sm w-full h-1/2 text-[#a199aa] ">Мы оповестим о нём подходящих исполнителей.</div>
+                    </div>
+                </div>
+            </div>
+            <div class="text-left h-full">
+                <div class="grid grid-cols-3 h-full">
+                    <div class="step w-full text-center h-full  text-8xl pt-4 text-[#a199aa]">2</div>
+                    <div class="w-full col-span-2">
+                        <div class="text-left text-xl font-bold pt-4 w-full h-1/2 ">Получите отклики</div>
+                        <div class="text-left text-sm w-full h-1/2 text-[#a199aa]">Заинтересованные исполнители предложат свои услуги.</div>
+                    </div>
+                </div>
+            </div>
+            <div class="text-left h-full">
+                <div class="grid grid-cols-3 h-full">
+                    <div class="step w-full text-center h-full  text-8xl pt-4 text-[#a199aa]">3</div>
+                    <div class="w-full col-span-2">
+                        <div class="text-left text-xl font-bold pt-4 w-full h-1/2 ">Выберите исполнителя</div>
+                        <div class="text-left text-sm w-full h-1/2 text-[#a199aa]">Обращайте внимание на проверенных исполнителей.</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
