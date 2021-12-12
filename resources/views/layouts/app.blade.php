5261e783faca06741df819190f3778c056ea7c5c
Farrux <farruxzokirov230596@gmail.com>
2021-12-12 16:50:53 +0500

    tasks location.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -23,5 +23,4 @@
 </body>
 <script src="{{asset('vendor/jquery/jquery.min.js')}}"></script>
 <script src="https://cdn.tailwindcss.com"></script>
-<script src="{{asset('vendor/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
 </html>
