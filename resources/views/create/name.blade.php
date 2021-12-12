733dcca407e88f8ecdd4a51eac8850fe14b781b8
Utkirbek <mutaliyevutkirbek@gmail.com>
2021-12-12 15:51:09 +0500

    changed

--- resources/views/create/name.blade.php
+++ resources/views/create/name.blade.php
@@ -11,7 +11,7 @@
     <!-- Information section -->
     <!-- <form class="" action="" method="post"> -->
 
-    Created Road map for Create a New Tast
+    {{--    Created Road map for Create a New Tast--}}
     <x-roadmap/>
     <div class="mx-auto w-2/3 my-16">
         <div class="grid grid-cols-3 h-full">
@@ -24,7 +24,8 @@
                 </div>
                 <div class="relative pt-1">
                     <div class="overflow-hidden h-2 text-xs flex rounded bg-gray-200 w-3/4 mx-auto ">
-                        <div style="width: 14%" class="shadow-none flex flex-col text-center whitespace-nowrap text-white justify-center bg-green-500"></div>
+                        <div style="width: 14%"
+                             class="shadow-none flex flex-col text-center whitespace-nowrap text-white justify-center bg-green-500"></div>
                     </div>
                 </div>
                 <div class="shadow-lg w-full h-96 mx-auto my-4 rounded-2xl	w-full">
@@ -36,7 +37,9 @@
                             <label class="block text-[#5f5869] text-sm mb-2" for="username">
                                 Название задания
                             </label>
-                            <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Например, нужен курьер на несколько доставок">
+                            <input
+                                class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                                id="username" type="text" placeholder="Например, нужен курьер на несколько доставок">
                             <div id="content" class="mt-4" style="display:none;">
                                 <label class="block text-left w-full mx-auto">
                                     <select class="form-select block w-5/12 float-left mt-1">
@@ -52,34 +55,35 @@
 
                             <div class="mt-4">
                                 <label class="text-sm mb-2">
-                                    <p id="showContent">Подкатегория <span class="underline hover:text-[#5f5869]">Другая посылка</span></p>
+                                    <p id="showContent">Подкатегория <span class="underline hover:text-[#5f5869]">Другая посылка</span>
+                                    </p>
                                 </label>
                             </div>
                         </div>
                     </div>
-                    <a href="/create/location"><input type="submit" class="bg-[#6fc727] hover:bg-[#5ab82e] w-11/12 ml-5 my-4 text-white font-bold py-2 px-4 rounded" name="" value="Далее"></a>
+                    <a href="/create/location"><input type="submit"
+                                                      class="bg-[#6fc727] hover:bg-[#5ab82e] w-11/12 ml-5 my-4 text-white font-bold py-2 px-4 rounded"
+                                                      name="" value="Далее"></a>
                 </div>
             </div>
 
-
-
-
-{{--            Created Component for Frequently Asked Questions--}}
-            <x-faq/>
+            <x-faq>
+            {{--            Created Component for Frequently Asked Questions--}}
+            </x-faq>
         </div>
     </div>
     <!-- </form> -->
 
     <script>
 
-        function addInput(){
+        function addInput() {
             var newdiv = document.createElement('div');
             //newdiv.id = dynamicInput[counter];
             newdiv.outerHTML = '';
             document.getElementById('formulario').appendChild(newdiv);
         }
 
-        function removeInput(btn){
+        function removeInput(btn) {
             btn.parentNode.remove();
         }
 
@@ -100,9 +104,14 @@
     </script>
 
     <script>
-        window.replainSettings = { id: '38d8d3f0-b690-4857-a153-f1e5e8b462a8' };
-        (function(u){var s=document.createElement('script');s.type='text/javascript';s.async=true;s.src=u;
-            var x=document.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);
+        window.replainSettings = {id: '38d8d3f0-b690-4857-a153-f1e5e8b462a8'};
+        (function (u) {
+            var s = document.createElement('script');
+            s.type = 'text/javascript';
+            s.async = true;
+            s.src = u;
+            var x = document.getElementsByTagName('script')[0];
+            x.parentNode.insertBefore(s, x);
         })('https://widget.replain.cc/dist/client.js');
     </script>
 
