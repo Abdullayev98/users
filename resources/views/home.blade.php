057757f8b7d7e2af3b6eebc17a26acde9d0b86c7
Farrux <farruxzokirov230596@gmail.com>
2021-12-12 14:46:02 +0500

    components layouts/app.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,67 +1,7 @@
-<!DOCTYPE html>
-<html lang="{{setting('language','en')}}" dir="ltr">
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>{{setting('app_name')}} | {{setting('app_short_description')}}</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="icon" type="image/png" href="{{$app_logo ?? ''}}"/>
-    <link rel="stylesheet" href="{{asset('vendor/fontawesome-free/css/all.min.css')}}">
-    <link href="https://fonts.googleapis.com/css?family=Poppins:300,400,600" rel="stylesheet">
-    <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="{{asset('dist/css/adminlte.min.css')}}">
-    <link rel="stylesheet" href="{{asset('css/styles.min.css')}}">
-</head>
-<body>
+@extends('layouts.app')
 
+@section('content')
 
-
-<div>
-    <div class="relative pt-6 px-4 sm:px-6 lg:px-8">
-        <nav class="relative flex items-center justify-between sm:h-10 lg:justify-start" aria-label="Global">
-            <div class="flex items-center flex-grow flex-shrink-0 lg:flex-grow-0">
-                <div class="flex items-center justify-between w-full md:w-auto">
-                    <a href="#">
-                        <img class="h-6 w-auto sm:h-10" src="https://assets.youdo.com/_next/static/media/logo.68780febe8ce798e440ca5786b505cd5.svg">
-                    </a>
-                    <div class="mr-2 flex items-center md:hidden">
-                        <button type="button" class="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500" aria-expanded="false">
-
-                            <!-- Heroicon name: outline/menu -->
-                            <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
-                            </svg>
-                        </button>
-                    </div>
-                </div>
-            </div>
-            <div class="hidden w-full md:inline-block md:ml-32 md:pr-4 lg:space-x-8 md:space-x-6 pt-0 mb-6">
-                <a href="/create/task" class="font-medium text-gray-500 hover:text-gray-900">Создать задание</a>
-
-                <a href="#" class="font-medium text-gray-500 hover:text-gray-900">Найти задания</a>
-
-                <a href="#" class="font-medium text-gray-500 hover:text-gray-900">Исполнители</a>
-                <a href="/home/profile" class="font-medium text-gray-500 hover:text-gray-900">profil</a>
-<!--
-                <a href="#" class="font-medium text-gray-500 hover:text-gray-900">Мои заказы</a>
--->
-{{--                <p class="text-center inline float-right md:float-none  "><a href="#" class="font-medium hover:text-yellow-500">Вход</a> или <a href="#" class="font-medium hover:text-yellow-500">регистрация</a></p>--}}
-
-            </div>
-            <p class="w-full text-right inline float-right md:float-none mb-6"><a href="#" class="font-medium hover:text-yellow-500" id="open-btn">Вход</a> или <a href="#" class="font-medium hover:text-yellow-500">Регистрация</a></p>
-        </nav>
-    </div>
-
-    <!--
-      Mobile menu, show/hide based on menu open state.
-
-      Entering: "duration-150 ease-out"
-        From: "opacity-0 scale-95"
-        To: "opacity-100 scale-100"
-      Leaving: "duration-100 ease-in"
-        From: "opacity-100 scale-100"
-        To: "opacity-0 scale-95"
-    -->
 
 <div class="bg-[url('https://assets.youdo.com/next/_next/static/images/frame-51209c6822214bfb9166eb41c4dec591.jpg')] bg-center bg-cover h-96 ">
     <div class="container-lg mx-auto">
@@ -186,9 +126,4 @@
     </script>
 
 
-<script src="{{asset('vendor/jquery/jquery.min.js')}}"></script>
-<script src="https://cdn.tailwindcss.com"></script>
-<script src="{{asset('vendor/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
-
-</body>
-</html>
+@endsection
