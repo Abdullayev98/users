1e5c139c472291129f47eec821de6cae5514866a
Utkirbek <mutaliyevutkirbek@gmail.com>
2021-12-13 10:20:26 +0500

    layout added to home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,20 +1,5 @@
 
-<!DOCTYPE html>
-<html lang="en" dir="ltr">
-<!-- <html lang="{{setting('language','en')}}" dir="ltr"> -->
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>{{setting('app_name')}} | {{setting('app_short_description')}}</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="icon" type="image/png" href="{{$app_logo ?? ''}}"/>
-    <link rel="stylesheet" href="{{asset('vendor/fontawesome-free/css/all.min.css')}}">
-    <link href="https://fonts.googleapis.com/css?family=Poppins:300,400,600" rel="stylesheet">
-    <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="{{asset('dist/css/adminlte.min.css')}}">
-    <link rel="stylesheet" href="{{asset('css/styles.min.css')}}">
-</head>
-<body>
+
 
 @extends('layouts.app')
 
