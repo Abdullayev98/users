cede530589f5078bc2216eb79a6e1f0f83aab83d
Utkirbek <mutaliyevutkirbek@gmail.com>
2021-12-14 14:57:41 +0500

    Service factory created and database filled with faker

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,5 +15,6 @@ class DatabaseSeeder extends Seeder
     {
         \App\Models\User::factory(10)->create();
         \App\Models\Task::factory(100)->create();
+        \App\Models\Service::factory(10)->create();
     }
 }
