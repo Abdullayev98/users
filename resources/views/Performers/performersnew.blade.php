eea788bdf473cc694262464821649c6eb6c0eeb4
narimonov <72002820+narimonov@users.noreply.github.com>
2021-12-16 12:49:56 +0500

    performers

---
+++ resources/views/Performers/performersnew.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="grid xl:grid-cols-3 grid-cols-1 w-10/12 mx-auto mt-16">
+
+      <div class="rounded text-sm bg-no-repeat xl:block hidden h-16 w-2/3 shadow-xl hover:shadow-md mx-auto">
+        <img src="https://css-static.youdo.com/assets/71201/i/man-with-shield-2adf66d1c569f7697c3dcb21bcbf695c.svg" class="float-left w-1/4 mr-2" alt="">
+        <p class="float-right w-2/3 py-auto">станьте исполнителем Юду. И начните зарабатывать</p>
+      </div>
+    <div class="xl:col-span-2">
+      <div class="w-full h-52 bg-[#f8f7ee]">
+        <h4></h4>
+      </div>
+    </div>
+</div>
+@endsection
