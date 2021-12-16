7854722d88802e6bbd5219b5a97e4dc8aee0d67d
Shamshod <62167469+Shamshod-Olimov@users.noreply.github.com>
2021-12-16 17:34:19 +0500

    user login/logout

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -53,7 +53,31 @@
                 </div>
                 <div class="mt-auto">
                     <div class="pt-6">
-                        <p class="w-4/12 text-right inline-block float-right text-gray-500 md:float-none mt-6 mb-6"><a href="#" id='open-btn' class="font-medium border-b border-black border-dotted hover:border-yellow-500 hover:text-yellow-500">Вход</a> или <a href="#" id="open-btn2" class=" border-b border-black border-dotted font-medium text-gray-500 hover:text-yellow-500 hover:border-yellow-500">Регистрация</a></p>
+                        @if (Route::has('login'))
+                            @auth
+                                <p class="w-full text-right inline-block float-right md:float-none mt-6 mb-6 mr-6">
+                                    <button href="#" class="font-medium hover:text-yellow-500">
+                                        <i class="far fa-bell"></i>
+                                    </button>
+                                    <button href="#" class="font-medium hover:text-yellow-500">
+                                        <i class="far fa-user"></i>
+                                    </button>
+                                </p>
+                            @else
+                                <p class="w-full text-right inline-block float-right md:float-none mt-6 mb-6">
+                                    <a href="{{ route('login') }}" class="font-medium hover:text-yellow-500">
+                                        Вход
+                                    </a>
+                                    или
+                                    <a href="{{ route('register') }}" class="font-medium hover:text-yellow-500">
+                                        Регистрация
+                                    </a>
+                                    <a href="{{ route('logout') }}" class="btn btn-danger">
+                                        Sign-out
+                                    </a>
+                                </p>
+                            @endauth
+                        @endif
 
                     </div>
                     <p class="my-4 text-xs text-center text-gray-400">
@@ -152,120 +176,34 @@
          <span class="inline-block">пополнить</span>
          </button> --}}
     </div>
-
-    <p class="w-full text-right inline-block float-right md:float-none mt-6 mb-6" >
-        <a  href="{{ route('login') }}" class="font-medium hover:text-yellow-500">
-            Вход
-        </a> 
-            или 
-        <a href="{{ route('register') }}" class="font-medium hover:text-yellow-500">
-            Регистрация
-        </a>
-        <a href="{{ route('logout') }}" class="btn btn-danger">
-            Sign-out
-        </a>
-    </p>
-
-</nav>
-{{-- login start --}}
-<div class="fixed hidden z-50 inset-0 bg-gray-600 bg-opacity-50 overflow-y-auto h-screen w-full" id="my-modal">
-    <div class="relative top-20 mx-auto p-5 border w-2/5 shadow-lg rounded-md bg-white">
-        <div class="mt-3 text-center">
-
-            <button type="submit" id="close-btn" class="px-4 py-4 bg-gray-300 rounded-md w-100 h-16 absolute right-4 top-4 hover:bg-gray-500">
-                <i class="fas fa-times text-white text-3xl w-full"></i>
-
-                <button type="submit" id="close-btn" class="px-4 py-4  rounded-md w-100 h-16 absolute right-4 top-4 ">
-                    <i class="fas fa-times text-2xl text-gray-600"></i>
+    @if (Route::has('login'))
+        @auth
+            <p class="w-full text-right inline-block float-right md:float-none mt-6 mb-6 mr-6">
+                <button href="#" class="font-medium hover:text-yellow-500 mr-3">
+                    <i class="far fa-bell"></i>
                 </button>
-                <div class="mx-auto flex items-center justify-center w-full">
-                    <h3 class="font-bold text-2xl block mb-4">
-                        Войти через:
-                    </h3>
-                </div>
-                <div class="mt-4">
-                    <button class="w-80 h-12 rounded-lg bg-red-500 text-gray-200 uppercase font-semibold hover:bg-red-700 text-gray-100 transition mb-4"> Google</button>
-                    <button class="w-80 h-12 rounded-lg bg-blue-500 text-gray-200 uppercase font-semibold hover:bg-blue-700 text-gray-100 transition mb-4"> Facebook</button>
-                </div>
-                <div class="mx-auto flex items-center justify-center w-full">
-                    <h3 class="font-bold text-2xl block mb-4 mt-4">
-                        Войти по электронной почте
-                    </h3>
-                </div>
-                <div>
-                    <div class="mb-4">
-                        <label class="block text-gray-500  text-sm " for="username">
-                            Электронная почта
-                        </label>
-                        <input class="shadow appearance-none border border-slate-300 rounded w-80 py-2 px-3 text-gray-700 mb-3 leading-tight hover:border-amber-500" id="username" type="text">
-                    </div>
-                    <div class="mb-6">
-                        <label class="block text-gray-500  text-sm " for="password">
-                            Пароль
-                        </label>
-                        <input class="shadow appearance-none border border-slate-300 rounded w-80 py-2 px-3 text-gray-700 mb-3 leading-tight hover:border-amber-500" id="password" type="password">
-                    </div>
-                </div>
-                <button id="ok-btn" class="w-80 h-12 rounded-lg bg-lime-500 text-gray-200 uppercase font-semibold hover:bg-lime-600 text-gray-100 transition mb-4">Войти</button>
-                <p class="text-sky-500 text-lg mb-4"><a href="#">Забыли пароль?</a></p>
+                <button href="#" class="font-medium hover:text-yellow-500 mr-3">
+                    <i class="far fa-user"></i>
+                </button>
+                <a href="{{ route('logout') }}" class="btn btn-danger">
+                    Sign-out
+                </a>
+            </p>
+        @else
+            <p class="w-full text-right inline-block float-right md:float-none mt-6 mb-6">
+                <a href="{{ route('login') }}" class="font-medium hover:text-yellow-500">
+                    Вход
+                </a>
+                или
+                <a href="{{ route('register') }}" class="font-medium hover:text-yellow-500">
+                    Регистрация
+                </a>
+            </p>
+        @endauth
+    @endif
+    
+</nav>
 
-
-                <p class="text-sm leading-6 text-gray-400">Сумма пополнения, минимум — 60 000сум</p>
-                <div class="mt-2 px-7 py-3">
-                    <input type="checkbox" class="w-5 h-5 rounded-md inline-block " />
-                    <p class="text-md inline-block ml-2">Оформить полис на 7 дней за 15 000 сум</p>
-                </div>
-                <div class="items-center px-4 py-3">
-                    <button id="ok-btn" class="px-4 py-2 bg-green-500 text-white text-xl font-medium rounded-md w-2/5 h-16  shadow-sm hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-green-300">
-                        К оплате x сум
-                    </button>
-                    <p>* — Порядок выплаты, ограничения и полные условия определены в <a href="/home/oferta" class="cursor-pointer text-sm text-blue-400 underline">Оферте</a></p>
-
-                    <p class="text-lg mb-4">Еще не с нами?<a class="text-sky-500" href="#">Зарегистрируйтесь</a></p>
-                </div>
-        </div>
-    </div>
-</div>
-</div>
-{{-- login end --}}
-
-{{-- register start --}}
-<div class="fixed hidden z-50 inset-0 bg-gray-600 bg-opacity-50 overflow-y-auto h-screen w-full" id="my-modal2">
-    <div class="relative top-20 mx-auto p-5 border w-2/5 shadow-lg rounded-md bg-white">
-        <div class="mt-3 text-center">
-            <button type="submit" id="close-btn2" class="px-4 py-4  rounded-md w-100 h-16 absolute right-4 top-4 ">
-                <i class="fas fa-times text-2xl text-gray-600"></i>
-            </button>
-            <div class="mx-auto flex items-center justify-center w-full">
-                <h3 class="font-bold text-2xl block mb-4">
-                    Быстрая регистрация через:
-                </h3>
-            </div>
-            <div class="mt-4">
-                <button class="w-80 h-12 rounded-lg bg-red-500 text-gray-200 uppercase font-semibold hover:bg-red-700 text-gray-100 transition mb-4"> Google</button>
-                <button class="w-80 h-12 rounded-lg bg-blue-500 text-gray-200 uppercase font-semibold hover:bg-blue-700 text-gray-100 transition mb-4"> Facebook</button>
-            </div>
-            <div class="mx-auto flex items-center justify-center w-full">
-                <h3 class="font-bold text-2xl block mb-4 mt-4">
-                    Или по электронной почте
-                </h3>
-            </div>
-            <div>
-                <div class="mb-4">
-                    <label class="block text-gray-500  text-sm " for="username">
-                        Электронная почта
-                    </label>
-                    <input class="shadow appearance-none border border-slate-300 rounded w-80 py-2 px-3 text-gray-700 mb-3 leading-tight hover:border-amber-500" id="username" type="text">
-                </div>
-            </div>
-            <button id="ok-btn" class="w-80 h-12 rounded-lg bg-lime-500 text-gray-200 uppercase font-semibold hover:bg-lime-600 text-gray-100 transition mb-4">Зарегистрироваться</button>
-            <p class="text-lg mb-4">Уже зарегистрированы?<a class="text-sky-500" href="#"> Войдите</a></p>
-        </div>
-    </div>
-</div>
-</div>
-</div>
-{{-- register end --}}
 
 {{-- pay modal start --}}
 <div class="fixed hidden z-50 inset-0 bg-gray-600 bg-opacity-50 overflow-y-auto h-full w-full" id="my-modal1">
