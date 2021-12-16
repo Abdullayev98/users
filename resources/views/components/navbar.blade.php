e63ffb3365258a90084ffea927d8d73f56ac90d2
narimonov <72002820+narimonov@users.noreply.github.com>
2021-12-16 11:39:02 +0500

    nav

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -11,6 +11,7 @@
                 <button class="navbar-burger flex items-center text-yellow-500 p-3">
                     <svg class="block h-4 w-4 fill-current" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                         <title>Mobile menu</title>
+                        
                         <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"></path>
                     </svg>
                 </button>
