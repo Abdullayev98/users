309e5d947ddb1c9f0caf0e4218c0bd45d5a4d441
FURYpmb <islombeknazarov98@gmail.com>
2021-12-16 17:59:12 +0500

    Navbar sign out

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -166,14 +166,8 @@
         </a>
     </p>
 
-    <p class="w-full text-right inline-block float-right md:float-none mt-6 mb-6">
-    <a  href="#" class="font-medium hover:text-yellow-500">
-            <i class="fal fa-bell"></i>
-        </a> 
-        <a href="#" class="font-medium hover:text-yellow-500">
-             <i class="fad fa-user-circle"></i>
-        </a>
-    </p>
+   
+
 
 </nav>
 {{-- login start --}}
