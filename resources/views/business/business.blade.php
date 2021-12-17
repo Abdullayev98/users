ffaf026632338ac3c471e00a5c6a8d08e6633243
asilbek20041207 <asilbek.pardabayev@bk.ru>
2021-12-17 19:42:38 +0500

    business page

---
+++ resources/views/business/business.blade.php
@@ -0,0 +1,265 @@
+<header class="h-[140px]" id="header">
+    <div class="md:flex md:justify-between grid-col-1 md:col-2 md:max-w-[1100px] mx-auto">
+        <div class="w-[700px] md:float-left md:w-4/12 pt-8">
+            <a href="/">
+                <img src="https://thumb.tildacdn.com/tild3432-3336-4166-a139-306139383666/-/resize/310x/-/format/webp/logo.png" alt="" class="w-[170px] mx-auto md:m-0">
+            </a>
+        </div>
+        <div class="md:w-8/12 hidden md:block md:ml-[200px] lg:ml-[350px] xl:ml-[450px]">
+            <img src="https://thumb.tildacdn.com/tild3461-3238-4161-b339-343236656266/-/resize/810x/-/format/webp/photo.png" alt="" class="w-[400px]">
+        </div>
+    </div>
+</header>
+<div class="w-12/12  overflow-hidden">
+    <div class="md:max-w-[1100px] mx-auto md:py-16">
+        <div class="md:relative z-10">
+            <div class="max-h-[100px]">
+                <p class="float-left mt-1">
+                    Совместно с &nbsp;
+                </p>
+                <img class="w-[35px]" src="https://thumb.tildacdn.com/tild3336-3538-4535-b939-373931323562/-/resize/64x/-/format/webp/min-hh-red.png" alt="">
+            </div>
+            <div>
+                <span class="md:text-[3rem] font-extrabold font-[Arial,sans-serif]">Помогаем бизнесу</span><br>
+                <span class="md:text-[3rem] font-extrabold font-[Arial,sans-serif]">работать с самозанятыми</span>
+            </div>
+            <div class="md:relative -top-[180px] -right-[550px] z-1">
+                <img src="https://thumb.tildacdn.com/tild6664-3164-4261-b435-663833636134/-/format/webp/1.png" alt="">
+            </div>
+            <div class="md:mt-10 md:relative md:-top-[600px]">
+                <div class="mb-5 text-[20px]">
+                    <img src="https://thumb.tildacdn.com/tild3633-6230-4539-b935-666465333066/-/format/webp/_1.png" alt="" class="float-left w-[35px]">
+                    <h1 class="font-[35px]">&nbsp;Находим исполнителей</h1>
+                </div>
+                <div class="mb-5 text-[20px]">
+                    <img src="https://thumb.tildacdn.com/tild3633-6230-4539-b935-666465333066/-/format/webp/_1.png" alt="" class="float-left w-[35px]">
+                    <h1 class="font-[35px]">&nbsp;Автоматизируем документооборот</h1>
+                </div>
+                <div class="mb-5 text-[20px]">
+                    <img src="https://thumb.tildacdn.com/tild3633-6230-4539-b935-666465333066/-/format/webp/_1.png" alt="" class="float-left w-[35px]">
+                    <h1 class="font-[35px]">&nbsp;Производим легальные расчеты</h1>
+                </div>
+            </div>
+            <div class="md:mt-10 md:relative -top-[200px] md:relative md:-top-[600px]">
+                <a type="button" href="#contact" class="text-white text-[18px] leading-[1.55] font-[500] bg-center border-transparent bg-[#5a66ff] rounded-[30px] py-3 px-8">Оставить заявку</a>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="md:relative md:-top-[400px]">
+    <div class="md:relative">
+        <img class="w-[60%] md:-mt-[100px] md:-ml-40" src="https://thumb.tildacdn.com/tild6363-3339-4562-b837-643138343233/-/format/webp/21.png" alt="">
+    </div>
+    <div class="md:relative md:-top-[400px] float-right md:-left-[100px] md:max-w-[800px]">
+        <h1 class="text-[2.5rem] md:w-[450px] font-semibold ">Ускорьте поиск исполнителей</h1>
+        <div class="mt-8">
+            <div class="mb-8">
+                <img class="w-[55px] float-left" src="https://thumb.tildacdn.com/tild6334-6539-4038-b135-663061623762/-/resize/112x/-/format/webp/_.png" alt="">
+                <h1 class="text-[20px]">Собственная база 2.6 млн исполнителей <br> для задач вашего бизнеса</h1>
+            </div>
+            <div class="mb-8">
+                <img class="w-[55px] float-left" src="https://thumb.tildacdn.com/tild6334-6539-4038-b135-663061623762/-/resize/112x/-/format/webp/_.png" alt="">
+                <h1 class="text-[20px]">Подбор самозанятых исполнителей по различным критериям</h1>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="w-md text-center">
+    <div class="text-[3.5rem] text-center font-[600] md:-mt-[360px]">Автоматизируйте <br> взаимодействие с самозанятыми</div>
+</div>
+
+<div class="md:w-[1400px] mx-auto">
+    <iframe class="w-[1100px] h-[600px] mx-auto relative md:-top-[500px] border rounded-md shadow-xl" src="https://player.vimeo.com/video/564205774?autoplay=1&loop=1&title=0&muted=1&autopause=0&background=1&byline=0&portrait=0" frameborder="0"></iframe>
+</div>
+<div class="md:flex grid-col-1 md:col-2 md:max-w-[1000px] mx-auto md:-mt-[450px]">
+    <div class="md:w-[300px] ml-4">
+        <img class="md:w-[55px] float-left" src="https://thumb.tildacdn.com/tild6239-3466-4230-a266-626535393136/-/resize/112x/-/format/webp/_.png" alt="">
+        <div class="text-[20px] ml-16">Помощь исполнителям в оформлении самозанятости</div>
+    </div>
+    <div class="md:w-[300px] mx-14">
+        <img class="md:w-[55px] float-left" src="https://thumb.tildacdn.com/tild6239-3466-4230-a266-626535393136/-/resize/112x/-/format/webp/_.png" alt="">
+        <div class="text-[20px] ml-16">Автоматическое формирование договоров и актов</div>
+    </div>
+    <div class="md:w-[300px]">
+        <img class="md:w-[55px] float-left" src="https://thumb.tildacdn.com/tild6239-3466-4230-a266-626535393136/-/resize/112x/-/format/webp/_.png" alt="">
+        <div class="text-[20px] ml-16">Актуальные статусы исполнителей в личном кабинете</div>
+    </div>
+</div>
+
+<div class="md:w-[1100px] mx-auto bg-[#d9effb] rounded-[30px] mt-20">
+    <div class="text-[34px] font-bold text-center pt-12">
+        Выберите удобный формат работы
+    </div>
+    <div class="md:flex md:justify-between py-[20px] px-[30px]">
+        <div class="md:w-[300px] md:mt-8">
+            <img src="https://thumb.tildacdn.com/tild6632-6361-4238-b462-383632613063/-/resize/566x/-/format/webp/1.png" alt="">
+            <div class="text-[24px] text-center font-bold pt-8">Личный кабинет</div>
+            <div class="text-[18px] text-center pt-4">
+                Работайте прямо из браузера, без дополнительных доработок на вашей стороне
+            </div>
+        </div>
+        <div class="md:w-[300px] md:mt-8">
+            <img src="https://thumb.tildacdn.com/tild6537-3366-4664-b362-306330306335/-/resize/546x/-/format/webp/2.png" alt="">
+            <div class="text-[24px] text-center font-bold pt-8">Аккаунт-менеджер</div>
+            <div class="text-[18px] text-center pt-4">
+                Получите персонального менеджера, который поможет с загрузкой данных и выплатами
+            </div>
+        </div>
+        <div class="md:w-[300px]">
+            <img src="https://thumb.tildacdn.com/tild3537-3863-4635-b034-386466633662/-/resize/492x/-/format/webp/3.png" alt="">
+            <div class="text-[24px] text-center font-bold pt-4">Интеграция по API</div>
+            <div class="text-[18px] text-center pt-4">
+                Подключите платформу к своей учетной системе для обмена данными
+            </div>
+        </div>
+    </div>
+    <div class="text-center py-8">
+        <a type="button" href="#contact" class="text-white text-[18px] leading-[1.55] font-[500] bg-center border-transparent bg-[#5a66ff] rounded-[30px] py-3 px-8">Оставить заявку</a>
+    </div>
+</div>
+
+<div class="mx-auto overflow-hidden mt-[80px]">
+    <div class="md:relative">
+        <div class="md:relative float-left md:-right-[210px] md:max-w-[800px]">
+            <h1 class="text-[2.5rem] md:w-[450px] font-bold ">Платите самозанятым без ограничений 24/7</h1>
+            <div class="mt-8">
+                <div class="mb-8">
+                    <img class="w-[55px] float-left" src="https://thumb.tildacdn.com/tild6334-6539-4038-b135-663061623762/-/resize/112x/-/format/webp/_.png" alt="">
+                    <h1 class="text-[20px]">Выплаты на любые карты,<br> реквизиты счета или по номеру телефона</h1>
+                </div>
+                <div class="mb-8">
+                    <img class="w-[55px] float-left" src="https://thumb.tildacdn.com/tild6334-6539-4038-b135-663061623762/-/resize/112x/-/format/webp/_.png" alt="">
+                    <h1 class="text-[20px]">Проверка лимитов и статуса<br> исполнителей перед выплатой</h1>
+                </div>
+                <div class="mb-8">
+                    <img class="w-[55px] float-left" src="https://thumb.tildacdn.com/tild6334-6539-4038-b135-663061623762/-/resize/112x/-/format/webp/_.png" alt="">
+                    <h1 class="text-[20px]">Автоматическое формирование <br> чеков и оплата налогов за самозанятого</h1>
+                </div>
+            </div>
+        </div>
+        <div class="md:relative md:-right-[240px]">
+            <img class="w-[70%]" src="https://thumb.tildacdn.com/tild3332-3961-4536-b864-653962313732/-/format/webp/31.png" alt="">
+        </div>
+    </div>
+</div>
+
+<div class="overflow-hidden mx-auto md:mt-16">
+    <div class="md:relative flex">
+        <div class="md:relative md:-ml-[100px] md:-mt-[50px]">
+            <img class="w-[90%]" src="https://thumb.tildacdn.com/tild6130-6362-4463-b235-663435346337/-/format/webp/4.png" alt="">
+            <div class="md:relative md:w-[400px] md:left-[300px] text-[12px] top-4">
+                <p class="text-[#9e9e9e]">* Страховые услуги оказывает ООО «Страховая компания «Манго». Лицензии С Л № 4372 и СИ № 4372 от 12.09.2019 г. Выданы Банком России без ограничения срока действия. Сайт: <a class="text-[#ff8562]" href="https://mango.rocks/">www.mango.rocks.</a> <a class="text-[#ff8562]" href="https://blog.youdo.com/mango">Подробнее о правилах и условиях страхования</a></p>
+                <p class="text-[#9e9e9e]">** Услуга по страхованию оказывается организацией Страховое акционерное общество «ВСК». Лицензия Банка России СИ № 0621 от 11.09.2015. <a class="text-[#ff8562]" href="https://youdo.com/vsk">Подробнее о правилах и условиях страхования</a></p>
+            </div>
+        </div>
+        <div class="md:relative top-14">
+            <h1 class="text-[2.5rem] md:w-[500px] font-bold font-['Radiance,sans-serif,Noto Sans']">Привлекайте лучших специалистов</h1>
+            <h1 class="text-[1.4rem] text-[#9e9e9e] md:w-[500px] font-bold font-['Radiance,sans-serif,Noto Sans']">А мы сделаем их работу комфортной</h1>
+
+            <div class="mt-10">
+                <div class="mb-8">
+                    <img class="w-[55px] float-left" src="https://thumb.tildacdn.com/tild3433-3337-4939-a164-643962313362/-/resize/112x/-/format/webp/_.png" alt="">
+                    <div class="font-semibold text-[24px] ml-16">Выплаты без ограничений</div>
+                    <h1 class="text-[20px] ml-16">Исполнитель может получать выплаты <br> за каждый заказ, день в день <br> Даже в выходные и праздничные дни</h1>
+                </div>
+                <div class="mb-10">
+                    <img class="w-[55px] float-left" src="https://thumb.tildacdn.com/tild3433-3337-4939-a164-643962313362/-/resize/112x/-/format/webp/_.png" alt="">
+                    <div class="font-semibold text-[24px] ml-16">Страховка от материального ущерба**</div>
+                    <h1 class="text-[20px] ml-16">Аналог больничного — поддержим исполнителя,<br> если он заболеет и не сможет работать</h1>
+                </div>
+                <div class="mb-10">
+                    <img class="w-[55px] float-left" src="https://thumb.tildacdn.com/tild3433-3337-4939-a164-643962313362/-/resize/112x/-/format/webp/_.png" alt="">
+                    <div class="font-semibold text-[24px] ml-16">Медицинская страховка*</div>
+                    <h1 class="text-[20px] ml-16">Если что-то пойдет не так, страховая может <br> выплатить до 100 000 рублей за ущерб,<br> причиненный застрахованным исполнителем</h1>
+                </div>
+                <div class="mb-10">
+                    <img class="w-[55px] float-left" src="https://thumb.tildacdn.com/tild3433-3337-4939-a164-643962313362/-/resize/112x/-/format/webp/_.png" alt="">
+                    <div class="font-semibold text-[24px] ml-16">Служба поддержки</div>
+                    <h1 class="text-[20px] ml-16">Поможем исполнителям получить статус самозанятого, познакомим с платформой и ответим на вопросы</h1>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="text-center mt-16 mb-10">
+        <a type="button" href="#contact" class="text-white text-[18px] leading-[1.55] font-[500] bg-center border-transparent bg-[#5a66ff] rounded-[30px] py-3 px-8">Узнай, что такое HR-Лидогенерация</a>
+    </div>
+</div>
+
+<div class="md:w-[1100px] mx-auto mt-20">
+    <div class="text-[34px] font-bold text-center pt-12 font-['Radiance,sans-serif,Noto Sans']">
+        Выберите удобный формат работы
+    </div>
+    <h1 class="text-[1.4rem] my-2 mb-8 text-[#9e9e9e] text-center font-bold font-['Radiance,sans-serif,Noto Sans']">Поделимся опытом, как это делать:</h1>
+    <div class="md:absolute md:w-[350px] md:ml-[750px]">
+        <a href="">
+            <img src="https://thumb.tildacdn.com/tild3063-6433-4561-b635-353731616635/-/resize/700x/-/format/webp/photo.png" alt="">
+        </a>
+    </div>
+    <div>
+        <div class="bg-[#e8f4e4] rounded-[20px] mb-8">
+            <img class="float-left w-[45px] m-6" src="https://thumb.tildacdn.com/tild6266-3935-4964-b133-316562343939/-/resize/68x/-/format/webp/photo.png" alt="">
+            <div class="p-8 text-[24px] ml-16">Участвуем в рабочих группах (при министерствах и <br>ведомствах) и бизнес- ассоциациях по развитию <br> рынка самозанятых</div>
+        </div>
+        <div class="bg-[#e8f4e4] rounded-[20px] mb-8">
+            <img class="float-left w-[45px] m-6" src="https://thumb.tildacdn.com/tild6266-3935-4964-b133-316562343939/-/resize/68x/-/format/webp/photo.png" alt="">
+            <div class="p-8 text-[24px] ml-16">Совместно с ФНС России, Рострудом и ОНФ <br> разработали памятку для юридических лиц по <br>безопасной работе с самозанятыми <br>
+                <a class="text-blue-500 mt-4" href="https://data.nalog.ru/html/sites/www.npd.nalog.ru/psz270820.pdf">Скачать на сайте ФНС →</a>
+            </div>
+        </div>
+        <div class="bg-[#e8f4e4] rounded-[20px]">
+            <img class="float-left w-[45px] m-6" src="https://thumb.tildacdn.com/tild6266-3935-4964-b133-316562343939/-/resize/68x/-/format/webp/photo.png" alt="">
+            <div class="p-8 text-[24px] ml-16">Совместно с ДПИР Москвы и мбм.мос.ру запустили обучающий онлайн курc для самозанятых исполнителей <br>
+                <a class="text-blue-500 mt-4" href="https://data.nalog.ru/html/sites/www.npd.nalog.ru/psz270820.pdf">Скачать на сайте ФНС →</a>
+            </div>
+        </div>
+    </div>
+    <div class="text-center py-8">
+        <a type="button" href="#contact" class="text-white text-[18px] leading-[1.55] font-[500] bg-center border-transparent bg-[#5a66ff] rounded-[30px] py-3 px-8">Оставить заявку</a>
+    </div>
+</div>
+
+
+
+
+
+
+
+
+
+<footer class="bg-black h-[250px] text-white">
+    <div class="md:max-w-[1400px] mx-auto">
+        <div class="md:flex md:justify-between py-12 text-bold">
+            <div class="">
+                © 2021 YouDo
+            </div>
+            <div class="inline">
+                <div class="text-bold -ml-20">
+                    <a class="ml-8" href="">О нас</a>
+                    <a class="ml-8" href="">Facebook</a>
+                    <a class="ml-8" href="">Twitter</a>
+                    <a class="ml-8" href="">Контакты</a>
+                    <a href="">HR-лидогенерация</a>
+                </div>
+            </div>
+            <div>
+                <a href="#header">Наверх</a>
+            </div>
+        </div>
+        <div class="max-w-xl mx-auto text-[#9e9e9e] text-[14px] text-center">
+            Информация, размещенная на настоящей странице, не является публичной офертой,
+            как она определена в Гражданском кодексе Российской Федерации
+        </div>
+    </div>
+</footer>
+
+
+
+
+
+
+<script src="https://cdn.tailwindcss.com"></script>
+<script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+
+</body></html>
