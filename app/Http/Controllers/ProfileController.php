fc27c5fb7052c2eafe48a44a8797e137c86d07b4
Abdullayev98 <79930634+Abdullayev98@users.noreply.github.com>
2021-12-18 16:23:29 +0500

    delete library 1

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -6,7 +6,6 @@ use App\Models\User;
 use App\Models\Social;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Storage;
 
 class ProfileController extends Controller
