f8eb9235f5a9fc7b027718b576da6a3dfd1ae8e9
AzizbekDev <36416819+AzizbekDev@users.noreply.github.com>
2021-12-21 10:05:53 +0500

    paynet transactions controller added

---
+++ app/Http/Controllers/PaynetTransactionController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\PaynetTransaction;
+
+class PaynetTransactionController extends Controller
+{
+    public function index()
+    {
+        //
+    }
+}
