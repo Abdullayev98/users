e3251d76681981ebe554690ad43d917464ff667e
narimonov <72002820+narimonov@users.noreply.github.com>
2021-12-22 18:30:43 +0500

    controller

--- app/Http/Controllers/PerformersController.php
+++ app/Http/Controllers/PerformersController.php
@@ -15,14 +15,12 @@ class PerformersController extends Controller
 {
     public function service(){
         $categories = DB::table('categories')->get();
-        $child_categories= DB::table('categories')->where('parent_id',$categories->id)->get();
+        $child_categories= DB::table('categories')->get();
         $users= User::where('role_id',2)->get();
         return view('Performers/performers',compact('child_categories','categories','users'));
     }
     public function performer($id){
         $users= User::where('id',$id)->get();
-        // $cat_id= User::where('id',$id)->get();
-
         return view('Performers/executors-courier',compact('users'));
     }
 }
