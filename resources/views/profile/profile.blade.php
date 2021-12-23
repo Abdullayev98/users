6717683020955c1697cacd08e5b039fa779938ef
Abram73 <abram4ik73@gmail.com>
2021-12-23 19:10:13 +0500

    hanged profile.blade.php

--- resources/views/profile/profile.blade.php
+++ resources/views/profile/profile.blade.php
@@ -13,9 +13,7 @@
                 <figure class="w-full">
                     <div class="top-0 right-0 float-right text-gray-500 text-sm">
                         <i class="far fa-eye"></i>
-                        @foreach($vcs as $vc)
-                        <span>{{$vc->count}} просмотров профиля</span>
-                        @endforeach
+                       
                     </div>
                     <br>
                     <h2 class="font-bold text-2xl mb-2">Здравствуйте, {{$user->name}}!</h2>
