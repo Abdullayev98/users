a8cb51be81d5af522d8d5488e705cf998f97c9d6
Abram <abram4ik73@gmail.com>
2021-12-23 16:35:09 +0500

    Performer filter by category_id

--- app/Http/Controllers/PerformersController.php
+++ app/Http/Controllers/PerformersController.php
@@ -8,7 +8,8 @@ use TCG\Voyager\Models\Category;
 use App\Models\User;
 use App\Models\UserView;
 use Session;
-
+use Illuminate\Support\Arr;
+use Illuminate\Support\Str;
 
 
 class PerformersController extends Controller
@@ -16,11 +17,28 @@ class PerformersController extends Controller
     public function service(){
         $categories = DB::table('categories')->get();
         $child_categories= DB::table('categories')->get();
-        $users= User::where('role_id',2)->get();
+        $users= User::where('role_id',2)->paginate(50);
         return view('Performers/performers',compact('child_categories','categories','users'));
     }
     public function performer($id){
         $users= User::where('id',$id)->get();
         return view('Performers/executors-courier',compact('users'));
     }
+
+public function perf_ajax($cf_id){
+    // $str = "1,2,3,4,5,6,7,8";
+    // $cat_arr = explode(",",$str);
+    //dd(array_search($id,$cat_arr));
+
+    // $users = User::where('role_id',2)->paginate(50);
+
+    // return $users;
+
+    $categories = DB::table('categories')->get();
+    $child_categories= DB::table('categories')->get();
+    $users= User::where('role_id',2)->paginate(50);
+    return view('Performers/performers_cat',compact('child_categories','categories','users','cf_id'));
+
+}
+
 }
