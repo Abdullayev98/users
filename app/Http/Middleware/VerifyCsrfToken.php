05d20d2d306b5aa39ce821a9b7be154a5742c321
AzizbekDev <36416819+AzizbekDev@users.noreply.github.com>
2021-12-25 18:39:17 +0500

    allow route without csrf token

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -12,10 +12,9 @@ class VerifyCsrfToken extends Middleware
      * @var array<int, string>
      */
     protected $except = [
-
+        '/handle/*',   
         '/paycom',
         '/prepare',
         '/complete'
-
     ];
 }
