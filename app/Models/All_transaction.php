48e19f9e2546b0fe23568fd5665eec0965f4540f
AzizbekDev <36416819+AzizbekDev@users.noreply.github.com>
2021-12-26 08:52:14 +0500

    Transaction model constants added

--- app/Models/All_transaction.php
+++ app/Models/All_transaction.php
@@ -8,10 +8,19 @@ use Illuminate\Database\Eloquent\Model;
 class All_transaction extends Model
 {
     use HasFactory;
-
-    const STATUS_NEW           = '0';
-    const STATUS_PAID_SUCCESS = '1';
-    const STATUS_PAID_REJECTED = '2';
+    const DRIVER_PAYME = 'Payme';
+    /** Pay in progress, order must not be changed. */
+    const STATE_WAITING_PAY  = 1;
+    /** Transaction completed and not available for sell. */
+    const STATE_PAY_ACCEPTED = 2;
+    /** Transaction is cancelled. */
+    const STATE_CANCELLED    = 3;
+    /** Transaction status is new. */
+    const STATUS_NEW         = 0;
+    /** Transaction status is paid success. */
+    const STATUS_SUCCESS     = 1;
+    /** Transaction status is rejected. */
+    const STATUS_REJECTED    = -1;
 
     protected $fillable = ['user_id','amount','method'];
 }
