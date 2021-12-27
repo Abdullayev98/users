9403b213b2161033c359b1bd3d8c3515c0e4a0c5
AzizbekDev <36416819+AzizbekDev@users.noreply.github.com>
2021-12-27 16:53:56 +0500

    paycom paid success change transaction status

--- app/Services/Payme/AllTransaction.php
+++ app/Services/Payme/AllTransaction.php
@@ -18,6 +18,14 @@ class AllTransaction extends Database
     /** Transaction is cancelled. */
     const STATE_CANCELLED = 3;
 
+    const STATUS_NEW         = 0;
+    /** Transaction status is paid success. */
+    
+    const STATUS_SUCCESS     = 1;
+    /** Transaction status is rejected. */
+    
+    const STATUS_REJECTED    = -1;
+    
     public $request_id;
     public $params;
 
@@ -165,13 +173,13 @@ class AllTransaction extends Database
     }
 
     /**
-     * Change order's state to specified one.
-     * @param int $state new state of the order
+     * Change transaction's state to specified one.
+     * @param int $state new state of the transaction
      * @return void
      */
     public function changeState($state)
     {
-        // todo: Implement changing order state (reserve order after create transaction or free order after cancel)
+        // todo: Implement changing transaction state (reserve order after create transaction or free transaction after cancel)
 
         // Example implementation
         $this->state = 1 * $state;
@@ -179,12 +187,23 @@ class AllTransaction extends Database
     }
 
     /**
-     * Check, whether order can be cancelled or not.
-     * @return bool true - order is cancellable, otherwise false.
+     * Change transaction's status to specified one.
+     * @param int $status new state of the transaction
+     * @return void
+     */
+    public function changeStatus($status)
+    {
+        $this->status = 1 * $status;
+        $this->save();
+    }
+
+    /**
+     * Check, whether transaction can be cancelled or not.
+     * @return bool true - transaction is cancellable, otherwise false.
      */
     public function allowCancel()
     {
-        // todo: Implement order cancelling allowance check
+        // todo: Implement transaction cancelling allowance check
 
         // Example implementation
         return true; // do not allow cancellation
