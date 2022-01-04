7b52b63f88d122a862f3cfdb2a80e9cc475ecde4
Utkirbek <mutaliyevutkirbek@gmail.com>
2022-01-04 11:59:17 +0500

    NewsAPIController modified

--- app/Http/Controllers/API/NewsAPIController.php
+++ app/Http/Controllers/API/NewsAPIController.php
@@ -6,32 +6,48 @@ use App\Http\Controllers\Controller;
 use App\Models\BlogNew;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use PHPUnit\Exception;
 
 class NewsAPIController extends Controller
 {
     public function index(){
-        $news = BlogNew::all();
-        $last2 = DB::table('blog_new')->orderBy('id', 'DESC')->first();
-        $data = ['news'=> $news, 'last2'=>$last2];
-        return response()->json($data);
+        try {
+            $news = BlogNew::all();
+            $last2 = DB::table('blog_new')->orderBy('id', 'DESC')->first();
+            $data = ['news'=> $news, 'last2'=>$last2];
+            return response()->json($data);
+        }catch (Exception $e){
+            $this->sendError($e->getMessage(), 200);
+            return response()->json(['status' => 'false', 'message' => $e->getMessage(),200]);
+        }
     }
     public function create(Request $request)
     {
-        $news = new BlogNew();
-        $image = $request->image;
+        try {
+            $news = new BlogNew();
+            $image = $request->image;
 
-        $imagename = "posts\December2021"."\/". $image->getClientOriginalExtension();
-        $request->image->move('storage\posts\December2021', $imagename);
-        $news->img = $imagename;
-        $news->title = $request->title;
-        $news->text = $request->title;
-        $news->save();
-        return response()->json(['status'=>'true','message'=>'created successfully']);
+            $imagename = "posts\December2021"."\/". $image->getClientOriginalExtension();
+            $request->image->move('storage\posts\December2021', $imagename);
+            $news->img = $imagename;
+            $news->title = $request->title;
+            $news->text = $request->title;
+            $news->save();
+            return response()->json(['status'=>'true','message'=>'created successfully']);
+        }catch (Exception $e){
+            $this->sendError($e->getMessage(), 200);
+            return response()->json(['status' => 'false', 'message' => $e->getMessage(),200]);
+        }
     }
     public function show($id){
-        $blog = DB::table('blog_new')->where('id',$id)->get();
-        $last3 = DB::table('blog_new')->orderBy('id', 'asc')->take(3)->get();
-        $data = ['blog'=>$blog,'last3'=>$last3];
-        return response()->json($data);
+        try {
+            $blog = DB::table('blog_new')->where('id',$id)->get();
+            $last3 = DB::table('blog_new')->orderBy('id', 'asc')->take(3)->get();
+            $data = ['blog'=>$blog,'last3'=>$last3];
+            return response()->json($data);
+        }catch (Exception $e){
+            $this->sendError($e->getMessage(), 200);
+            return response()->json(['status' => 'false', 'message' => $e->getMessage(),200]);
+        }
     }
 }
