b12fa9262acefbfa67e68edb9325cff0b89380ef
Utkirbek <mutaliyevutkirbek@gmail.com>
2022-01-06 14:15:28 +0500

    navbar.blade.php is added to the multilanguage

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -146,10 +146,10 @@
                 <span></span>
             </button>
             <ul class="bg-white border rounded-md transform scale-0 group-hover:scale-100 absolute transition duration-150 ease-in-out origin-top ">
-                @foreach (\TCG\Voyager\Models\Category::query()->where('parent_id', null)->get() as $category)
+                @foreach (\TCG\Voyager\Models\Category::withTranslations(['ru', 'uz'])->where('parent_id', null)->get() as $category)
                     <li class="py-2 px-4 rounded-sm hover:bg-gray-200">
                         <button class="w-full text-left flex items-center outline-none focus:outline-none">
-                            <span class="pr-1 flex-1">{{ $category->name }}</span>
+                            <span class="pr-1 flex-1">{{ $category->getTranslatedAttribute('name',Session::get('lang') , 'fallbackLocale') }}</span>
                             <span class="mr-auto">
                                 <svg class="fill-current h-4 w-4 transition duration-150 ease-in-out" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
                                     <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
@@ -158,10 +158,10 @@
                         </button>
                         <ul class="bg-white border rounded-sm absolute top-0 right-0 transition duration-150 ease-in-out origin-top-left w-100">
 
-                            @foreach (\TCG\Voyager\Models\Category::query()->where('parent_id', $category->id)->get() as $category2)
+                            @foreach (\TCG\Voyager\Models\Category::withTranslations(['ru', 'uz'])->where('parent_id', $category->id)->get() as $category2)
                                 <li class="rounded-sm">
                                     <a class=" py-3 px-5 w-full block hover:bg-gray-100" href="/task/create?category_id={{ $category2->id }}">
-                                        {{ $category2->name }}
+                                        {{ $category2->getTranslatedAttribute('name',Session::get('lang') , 'fallbackLocale') }}
                                     </a>
                                 </li>
                             @endforeach
