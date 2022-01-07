d0f33410d42ab9cec65c335f8300e80fc7411332
narimonov <narimanovr1404@gmail.com>
2022-01-07 11:15:45 +0300

    Controller

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -27,7 +27,6 @@ class Controller extends BaseController
         $howitworks = How_work_it::all();
         if (!session()->has('lang')) {
             Session::put('lang', 'ru');
-
         }
         return view('home',compact('tasks','howitworks', 'categories'));
     }
