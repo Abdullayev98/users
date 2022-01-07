28a432b86859f2ca11936e585fdbec766c33cb88
arabic0513 <rustamyusupov230@gmail.com>
2022-01-07 16:00:04 +0500

    Login Register bug fix & test

--- resources/views/auth/signin.blade.php
+++ resources/views/auth/signin.blade.php
@@ -21,16 +21,16 @@
                 <form method="POST" action="{{ route('signin.custom') }}">
                     @csrf
                     <div class="mb-4">
-                        <label class="block text-gray-500  text-sm " for="email">
-                            Электронная почта
+                        <label class="block text-gray-500  text-sm " for="name">
+                            Имя Фамилия
                         </label>
-                        <input type="text" name="email" placeholder="Email" id="email" 
-                        class="shadow appearance-none border border-slate-300 rounded 
+                        <input type="text" name="name" placeholder="Name" id="name"
+                        class="shadow appearance-none border border-slate-300 rounded
                         w-80 py-2 px-3 text-gray-700 mb-3 leading-tight hover:border-amber-500"
                         required autofocus>
 
-                        @if ($errors->has('email'))
-                            <span class="text-danger">{{ $errors->first('email') }}</span>
+                        @if ($errors->has('name'))
+                            <span class="text-danger">{{ $errors->first('name') }}</span>
                         @endif
                     </div>
                     <div class="mb-6">
@@ -38,15 +38,15 @@
                             Пароль
                         </label>
                         <input   type="password" name="password" placeholder="Password" id="password" required
-                        class="shadow appearance-none border border-slate-300 rounded w-80 py-2 px-3 
+                        class="shadow appearance-none border border-slate-300 rounded w-80 py-2 px-3
                         text-gray-700 mb-3 leading-tight hover:border-amber-500">
-                        
+
                         @if ($errors->has('password'))
                             <span class="text-danger">{{ $errors->first('password') }}</span>
                         @endif
                     </div>
                     <button type="submit"
-                        class="w-80 h-12 rounded-lg bg-lime-500 text-gray-200 uppercase 
+                        class="w-80 h-12 rounded-lg bg-lime-500 text-gray-200 uppercase
                         font-semibold hover:bg-lime-600 text-gray-100 transition mb-4">
                         Войти
                     </button>
@@ -64,5 +64,5 @@
                 </p>
             </div>
         </div>
-    
+
 @endsection
