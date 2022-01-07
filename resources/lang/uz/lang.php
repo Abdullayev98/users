db9d0ccff6308ae3e05db0e49a81ad8e13eb071d
narimonov <narimanovr1404@gmail.com>
2022-01-07 15:55:16 +0300

    lang

---
+++ resources/lang/uz/lang.php
@@ -0,0 +1,8 @@
+<?php
+  
+return [
+  
+    'header_title' => "Biz sizni tashvishlardan xalos qilamiz",
+    'header_sub' => "Biz har qanday vazifa uchun ishonchli pudratchi topishga yordam beramiz",
+  
+];
