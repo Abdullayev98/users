7864d466e0347ef2c12abe6c0b4f0eba1ff1e203
narimonov <narimanovr1404@gmail.com>
2022-01-08 18:41:37 +0300

    detailed tasks blade

--- resources/views/task/detailed-tasks.blade.php
+++ resources/views/task/detailed-tasks.blade.php
@@ -139,7 +139,7 @@
                                     </div>
                                 </div>
                             </div>
-{{--                            <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js" defer></script>--}}
+                           {{-- <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js" defer></script> --}}
                         </div>
                     </div>
 
