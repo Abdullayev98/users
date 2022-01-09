31e480086e983db34140fc69ef3df87bc45684bc
Asadmstx <asadmstx19@gmail.com>
2022-01-09 15:37:27 +0500

    lang Ru finished

--- resources/views/create/date.blade.php
+++ resources/views/create/date.blade.php
@@ -13,10 +13,10 @@
 <div class="grid grid-cols-3 gap-x-20">
   <div class="md:col-span-2 col-span-3">
     <div class="w-full text-center text-2xl">
-      Ищем исполнителя для задания "{{session('name')}}"
+      @lang('lang.budget_lookingFor') "{{session('name')}}"
     </div>
     <div class="w-full text-center my-4 text-[#5f5869]">
-      Задание заполнено на 57%
+      @lang('lang.date_percent')
     </div>
     <div class="pt-1">
       <div class="overflow-hidden h-1 text-xs flex rounded bg-gray-200  mx-auto ">
@@ -25,10 +25,10 @@
     </div>
     <div class="shadow-2xl w-full md:p-16 p-4 mx-auto my-4 rounded-2xl	w-full">
       <div class="py-4 mx-auto px-auto text-center text-3xl texl-bold">
-        Когда нужно приступить к работе?
+        @lang('lang.date_startTime')
       </div>
       <div class="py-4 mx-auto px-auto text-center text-sm texl-bold">
-        Укажите точную дату или период, когда нужно приступить к работе.
+        @lang('lang.date_startDate')
       </div>
       <div class="py-4 mx-auto  text-left ">
         <div class="mb-4">
@@ -36,9 +36,9 @@
 
             <div class="flex items-center rounded-lg border py-1">
                   <select name="start[]" id="periud" class="form-select appearance-none block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding bg-no-repeat rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:outline-none" aria-label="Default select example">
-                      <option selected value="Начать работу" id="1">Начать работу</option>
-                      <option value="Закончить работу" id="2">Закончить работу</option>
-                      <option value="Указать период" id="3">Указать период</option>
+                      <option selected value="Начать работу" id="1">@lang('lang.date_startTask')</option>
+                      <option value="Закончить работу" id="2">@lang('lang.date_finishTask')</option>
+                      <option value="Указать период" id="3">@lang('lang.date_givePeriod')</option>
                   </select>
             </div>
             <div class="flex items-center rounded-lg border py-1">
@@ -54,12 +54,12 @@
              <div class="flex w-full gap-x-4 mt-4">
                <a href="/task/create/location" class="w-1/3  border border-[#000]-700 hover:border-[#000] transition-colors rounded-lg py-2 text-center flex justify-center items-center gap-2">
               <!-- <button type="button"> -->
-                 Назад
+                 @lang('lang.budget_back')
                <!-- </button> -->
              </a>
                <input type="submit"
                                class="bg-[#6fc727] hover:bg-[#5ab82e] w-2/3 cursor-pointer text-white font-bold py-5 px-5 rounded"
-                               name="" value="Далее">
+                               name="" value="@lang('lang.name_next')">
 
              </div>
 
