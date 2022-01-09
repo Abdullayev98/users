bb6bb664f349800e5057c30b997ee1d416013a2d
narimonov <narimanovr1404@gmail.com>
2022-01-09 10:34:39 +0300

    Reponse to task full

--- resources/views/task/detailed-tasks.blade.php
+++ resources/views/task/detailed-tasks.blade.php
@@ -137,7 +137,7 @@
                                     </div>
                                 </div>
                             </div>
-                           {{-- <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js" defer></script> --}}
+                           <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js" defer></script>
                         </div>
                     </div>
 
