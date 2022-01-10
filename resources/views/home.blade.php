cd0e55915418c86566296103ed6bb83b2c197627
Kamron <83412736+kamronrizayev@users.noreply.github.com>
2022-01-10 16:02:50 +0500

    how it works translation

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -179,15 +179,15 @@
                             alt="">
                     </div>
                     <div class="text-left">
-                        <h3 class="md:text-4xl text-[24px] font-semibold my-8"> {{$cnt_for_hiw + 1}}.{{$howitwork->title}}</h3>
+                        <h3 class="md:text-4xl text-[24px] font-semibold my-8">{{ $howitwork->getTranslatedAttribute('title',Session::get('lang') , 'fallbackLocale') }}</h3>
                         <a href="/categories/1" class="text-blue-500 underline text-[22px]">@lang('lang.body_createTask')</a>
                     </div>
 
 @else
 
                     <div class="text-left my-16 md:block hidden">
-                        <h3 class="text-4xl font-semibold my-8"> {{$cnt_for_hiw + 1}}.{{$howitwork->title}}</h3>
-                        {!!$howitwork->description!!}
+                        <h3 class="text-4xl font-semibold my-8">{{ $howitwork->getTranslatedAttribute('title',Session::get('lang') , 'fallbackLocale') }}</h3>
+                        {{ strip_tags($howitwork->getTranslatedAttribute('description',Session::get('lang') , 'fallbackLocale')) }}
                     </div>
                     <div class="my-16 md:block hidden">
                         <img
@@ -201,7 +201,7 @@
                             alt="">
                     </div>
                     <div class="text-left md:hidden block">
-                        <h3 class="text-2xl font-semibold mt-8"> {{$cnt_for_hiw + 1}}.{{$howitwork->title}}</h3>
+                        <h3 class="text-2xl font-semibold mt-8">{{ $howitwork->getTranslatedAttribute('title',Session::get('lang') , 'fallbackLocale') }}</h3>
                         <a href="/categories/1" class="text-blue-500 underline text-[22px]">@lang('lang.body_createTask')</a>
                     </div>
 @endif
