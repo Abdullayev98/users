4755bdae1fe33c109bdcddc7b19170294ab486d0
narimonov <narimanovr1404@gmail.com>
2022-01-15 19:18:22 +0300

    search task js

--- public/js/search_tasks.js
+++ public/js/search_tasks.js
@@ -148,7 +148,7 @@ $(".address").keyup(function() {
         count = 0;
 
     // Loop through the comment list
-    $('#results p').each(function() {
+    $('#results .location').each(function() {
         // If the list item does not contain the text phrase fade it out
         if ($(this).text().search(new RegExp(filter, "i")) < 0) {
             var parent = $(this).parent();
