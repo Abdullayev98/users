5636194edc8da0dc11cd2a54b623366e0a74ea3d
narimonov <narimanovr1404@gmail.com>
2022-01-16 22:44:03 +0300

    search task js

--- resources/views/task/search.blade.php
+++ resources/views/task/search.blade.php
@@ -158,8 +158,8 @@
                             <div class="b-tasks-sorting">
                                 <div class="inline-flex items-center my-5">
                                     <span class="title__994cd">@lang('lang.search_filter')</span>
-                                    <a href="/task-search" data-sort-type="1" class="mx-5">@lang('lang.search_byDate')</a>
-                                    <button data-sort="nomer"  data-sort-type="2"  class="srt-nomer focus:outline-none mx-5 active">@lang('lang.search_byHurry')</button>
+                                    <a href="/task-search" class="mx-5">@lang('lang.search_byDate')</a>
+                                    <button id="srochnost" class=" focus:outline-none mx-5 active">@lang('lang.search_byHurry')</button>
                                     <button id="as" data-sort-type="3"  class="mx-5 ">@lang('lang.search_byRemote')</button>
                                 </div>
                             </div>
