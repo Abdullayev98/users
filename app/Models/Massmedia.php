f336f7be9bc2efe024f09df77635c565383c468d
Asadmstx <asadmstx19@gmail.com>
2022-01-22 11:45:46 +0500

    massmedia translate

--- app/Models/Massmedia.php
+++ app/Models/Massmedia.php
@@ -4,8 +4,10 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use TCG\Voyager\Traits\Translatable;
 
 class Massmedia extends Model
 {
     use HasFactory;
+    use Translatable;
 }
