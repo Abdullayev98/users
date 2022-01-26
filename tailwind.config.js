a21d97e29c119d4915f0ea4d6be7b2d6f58748e3
umarovjamshid88 <umarovjamshid88@gmail.com>
2022-01-26 19:32:47 +0500

    date.blade date change

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,21 @@
     module.exports = {
-    theme: {
-        extend: {
-            backgroundImage: {
-                'hero-pattern': "url('{{asset('/images/download_hand_User.png')}}')",
-                'footer-texture': "url('https://wallpapercave.com/wp/wp4002616.jpg')",
+        theme: {
+            extend: {
+                backgroundImage: {
+                    'hero-pattern': "url('{{asset('/images/download_hand_User.png')}}')",
+                    'footer-texture': "url('https://wallpapercave.com/wp/wp4002616.jpg')",
+                },
+                textColor: {
+                    'primary': '#3490dc',
+                    'secondary': '#ffed4a',
+                    'danger': '#e3342f',
+                    'whiteblue': '#80e6ff';
+                }
             }
-        textColor: {
-        'primary': '#3490dc',
-        'secondary': '#ffed4a',
-        'danger': '#e3342f',
-        'whiteblue': '#80e6ff';
-            }
-        }
-}
+        },
+        plugins: [
+            require('@tailwindcss/forms')({
+                strategy: 'class',
+            }),
+        ],
+    }
