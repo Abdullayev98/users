4ee94b050aaac9e180381e7f0e43740cea7c632f
Avazbek Nematov <68123420+avacoderuz@users.noreply.github.com>
2022-01-29 11:20:43 +0500

    search faq bug fix by Rahimjon

--- app/Http/Controllers/FaqsController.php
+++ app/Http/Controllers/FaqsController.php
@@ -8,16 +8,20 @@ use App\Models\Faqs;
 
 class FaqsController extends Controller
 {
-        public function index()
-        {
-            $fc = FaqCategories::withTranslations(['ru', 'uz'])->get();
-            return view('faq.faq',compact('fc'));
-        }
+    public function index(Request $request)
+    {
+        $fc = FaqCategories::all();
 
-         public function questions($id)
-        {
-          $fq = Faqs::withTranslations(['ru', 'uz'])->where('category_id',$id)->get();
-          $fc = FaqCategories::withTranslations(['ru', 'uz'])->where('id',$id)->first();
-            return view('faq.faq-ans', compact('fq','fc'));
-        }
+        if ($request->input('search')) {
+            $fc = FaqCategories::where('title','like', '%'.$request->input('search')."%")->get();
+        };
+        return view('faq.faq', compact('fc'));
+    }
+
+    public function questions($id)
+    {
+        $fq = Faqs::withTranslations(['ru', 'uz'])->where('category_id', $id)->get();
+        $fc = FaqCategories::withTranslations(['ru', 'uz'])->where('id', $id)->first();
+        return view('faq.faq-ans', compact('fq', 'fc'));
+    }
 }
--- routes/web/shohruz.php
+++ routes/web/shohruz.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\FaqsController;
 
-Route::get('/faq',[FaqsController::class, 'index']);
+Route::get('/faq',[FaqsController::class, 'index'])->name('faq.index');
 Route::get('/questions/{id}', [FaqsController::class,'questions'])->name('questions');
 
 ?>
