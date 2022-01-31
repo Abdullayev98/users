70b1cbf2b62b065bfc554d68cb9e8da95cda20b0
narimonov <narimanovr1404@gmail.com>
2022-01-31 18:07:12 +0300

    scroll to topFunction

--- resources/views/task/search.blade.php
+++ resources/views/task/search.blade.php
@@ -339,8 +339,8 @@
 
         // When the user clicks on the button, scroll to the top of the document
         function topFunction() {
-            document.body.scrollTop = 0;
-            document.documentElement.scrollTop = 0;
+            $('html,body').animate({ scrollTop: 0 }, 'slow');
+
         }
     </script>
 
