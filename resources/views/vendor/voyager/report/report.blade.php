210c4f01dede736d7309aa707e400149ed34f124
arabic0513 <rustamyusupov230@gmail.com>
2022-01-31 22:36:41 +0500

    Report Full

--- resources/views/vendor/voyager/report/report.blade.php
+++ resources/views/vendor/voyager/report/report.blade.php
@@ -11,27 +11,56 @@
                         <div class="table-responsive">
                             <table id="dataTable" class="table table-hover">
                                 <thead>
-                                    <tr>
-                                        Reports
+                                <tr>
+                                        @foreach( $columns as $get_name_of )
+                                            <th class="no-sort no-click bread-actions">
+                                                {{ $get_name_of->Field }}
+                                            </th>
+                                        @endforeach
                                     </tr>
                                 </thead>
                                 <tbody>
+                                @foreach($task_parent as $parent)
+                                @php
+                                    $categories_array = \App\Models\Category::where('parent_id', $parent->id)->pluck('id')->toarray();
+                                    $category_count = \App\Models\Task::whereIn('category_id', $categories_array)->count();                                        
+                                    $categories_array1 = \App\Models\Category::where('parent_id', $parent->id)->pluck('id')->toarray();
+                                    $category_count1 = \App\Models\Task::whereIn('category_id', $categories_array1)->pluck('budget')->toArray();                                        
+                                    $budgets = str_replace(array('до', 'сум', 'от'), '', $category_count1);
+                                    $all_budget = array_sum($budgets);                                
+                                @endphp
                                     <tr>
-                                        @foreach( $columns as $get_name_of )
-                                            <td class="no-sort no-click bread-actions">
-                                                {{ $get_name_of->Field }}
-                                            </td>
-                                        @endforeach
+                                        <td>
+                                            {{$parent->id}}
+                                        </td>
+                                        <td>
+                                            {{$parent->name}}
+                                        </td>
+                                        <td>
+                                            <!-- @if($parent->parent_id == !null)
+                                                @foreach($parent->tasks as $task)
+                                                    {{ $task->name }}
+                                                @endforeach
+                                            @endif -->
+                                            
+                                            {{$category_count}}
+                                        </td>
+                                        <td>
+                                        {{$all_budget}}
+                                        </td>
+                                        <td>
+                                            {{$parent->created_at}}
+                                        </td>
+                                        <td>
+                                            {{$parent->updated_at}}
+                                        </td>
                                     </tr>
+                                    @endforeach
                                 </tbody>
                             </table>
                         </div>
                         <div class="pull-left">
-                        @foreach($task_parent as $parent_name)
-                                <div role="status" class="show-res" aria-live="polite">
-                                    {{ $parent_name->id }} {{ $parent_name->name }}
-                                </div>
-                        @endforeach
+                        
                     </div>
                         <div class="pull-right">
                             </div>
