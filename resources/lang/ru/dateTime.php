107ccae875a78b7a7682adee4f236c1438f15e4d
umarovjamshid88 <umarovjamshid88@gmail.com>
2022-01-31 13:51:42 +0500

    date.blade update

---
+++ resources/lang/ru/dateTime.php
@@ -0,0 +1,10 @@
+<?php
+
+
+return [
+    "start_date.required" => 'Не указали месяц',
+    "start_date.date" => 'Не указали месяц',
+    "end_date.required" => 'Не указали месяц',
+    "end_date.date" => 'Не указали месяц',
+];
+
