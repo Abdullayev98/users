255c9444b52f34606a64281c5708179667f9c712
arabic0513 <rustamyusupov230@gmail.com>
2022-02-06 17:26:04 +0500

    Bug fix Portfolio

--- resources/views/profile/portfolio.blade.php
+++ resources/views/profile/portfolio.blade.php
@@ -8,10 +8,14 @@
                 $images = explode(',', $comments->image);
                 $a = array($images);
             @endphp
-        @for($i = 0; $i <= sizeof($a); $i++)
-            <img src="{{asset($images[$i])}}" alt="Images not found">
-            {{--@dd($image);--}}
-        @endfor
+        @if(sizeof($images) != 1)
+            @for($i = 0; $i <= sizeof($a); $i++)
+                <img src="{{asset($images[$i])}}" alt="Images not found">
+                {{--@dd($image);--}}
+            @endfor
+        @else
+                <img src="{{asset($images[0])}}" alt="Images not found">
+        @endif
         @endforeach
     </div>
 @endsection
