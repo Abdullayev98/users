0184426340a1da652e2caff42d7f3375212bb75e
Avazbek Nematov <68123420+avacoderuz@users.noreply.github.com>
2022-02-11 16:47:15 +0500

    custom fields in routes part #1

---
+++ app/Services/Task/CreateService.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Services\Task;
+
+use App\Models\CustomFieldsValue;
+use Illuminate\Support\Arr;
+
+class CreateService
+{
+
+    public function syncCustomFields($task){
+        $task->custom_field_values()->delete();
+        $this->attachCustomFields($task);
+
+    }
+
+
+    public function attachCustomFields($task){
+        foreach ($task->category->custom_fields as $data) {
+            $value = new CustomFieldsValue();
+            $value->task_id = $task->id;
+            $value->custom_field_id = $data->id;
+            $arr = Arr::get(request()->all(), $data->name);
+            $value->value = is_array($arr) ? json_encode($arr) : $arr;
+            $value->save();
+        }
+    }
+    public function attachCustomFieldsByRoute($task, $routeName){
+        foreach ($task->category->custom_fields()->where('route',$routeName)->get() as $data) {
+            $value = new CustomFieldsValue();
+            $value->task_id = $task->id;
+            $value->custom_field_id = $data->id;
+            $arr = Arr::get(request()->all(), $data->name);
+            $value->value = is_array($arr) ? json_encode($arr) : $arr;
+            $value->save();
+        }
+    }
+}
