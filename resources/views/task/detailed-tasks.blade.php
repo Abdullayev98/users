fa3ce528dd767c54832f09e02757796a3e3ecd29
narimonov <narimanovr1404@gmail.com>
2022-02-12 10:26:52 +0300

    detailed tasks

--- resources/views/task/detailed-tasks.blade.php
+++ resources/views/task/detailed-tasks.blade.php
@@ -14,7 +14,7 @@
                             <div class="md:flex flex-row">
                                 <p class="py-2 md:px-3 bg-amber-200 text-black-500 rounded-lg">{{$task->budget}}</p>
                                 @auth()
-                                    @if($task->user_id == auth()->user()->id)
+                                    @if($task->user_id == auth()->user()->id && $task->status < 2)
                                         <a href="{{ route('task.changetask', $task->id) }}"
                                            class="py-2 px-2 text-gray-500 hover:text-red-500">
                                             <i class="fas fa-pencil-alt"></i>
@@ -41,7 +41,7 @@
                                 @endif
                                 <p class="mr-3 md:pl-2 pr-3 md:border-r-2 border-gray-400">{{$task->created_at}}</p>
                                 <p class="pr-3 ">{{ $task->category->getTranslatedAttribute('name',Session::get('lang') , 'fallbackLocale') }}</p>
-                                @if($task->user_id == auth()->id() )
+                                @if($task->user_id == auth()->id() && $task->status < 2)
                                     <form action="{{route("delete.task", $task->id)}}" method="post">
                                         @csrf
                                         @method('delete')
