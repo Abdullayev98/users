64c212b778515f3689b5759bf7063a16b200a5e5
Ozodbek1405 <roziyevozodbek75@gmail.com>
2022-02-17 18:34:36 +0500

    choosetasks.blade.php

--- resources/views/task/choosetasks.blade.php
+++ resources/views/task/choosetasks.blade.php
@@ -7,7 +7,7 @@
     <div class="container w-10/12 mx-auto text-base">
         <div class="w-10/12 md:w-8/12 mx-auto text-center">
             <h1 class="text-3xl pt-5 md:text-5xl font-bold">@lang('lang.chT_chooseCat')</h1>
-            <h3 class="text-xl md:text-2xl my-5 text-gray-500">@lang('lang.chT_weHelp')</h3>
+            <h3 class="text-xl my-5 font-semibold text-gray-400">@lang('lang.chT_weHelp')</h3>
             <div class="max-w-full container mx-auto md:hidden">
                 <div class="slider" >
                     <div class="slider__wrapper" >
@@ -32,9 +32,9 @@
             <div class="hidden md:block">
                 @foreach($categories as $category)
                     <button type="button"
-                            class="bg-inherit hover:bg-yellow-300 border py-1 rounded-full px-4 my-4 text-gray-500 text-left md:text-center text-md md:inline-block block">
+                            class="bg-inherit hover:text-yellow-600 border py-1 rounded-full px-4 my-4 text-gray-500 border-gray-300 text-left md:text-center text-md md:inline-block block">
                         <i class="fas {{ $category->ico }}"></i>
-                        <a href="{{route('categories',['id'=>$category->id])}}">
+                        <a class="text-sm" href="{{route('categories',['id'=>$category->id])}}">
                             {{$category->getTranslatedAttribute('name',Session::get('lang') , 'fallbackLocale')}}
                         </a>
                     </button>
