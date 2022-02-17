83b21217544b37cf5999ede9e4840b73cda15a88
asilbek20041207 <asilbek.pardabayev@bk.ru>
2022-02-17 11:05:12 +0500

    budget bugs

--- resources/views/create/budget.blade.php
+++ resources/views/create/budget.blade.php
@@ -46,7 +46,7 @@
                                                             </div>
                                                         </div>
                                                     </div>
-                                                    <div id="tooltip-light" role="tooltip" class="inline-block absolute visible z-30 py-2 px-1 text-sm font-medium text-gray-900 bg-white rounded-lg border border-gray-200 shadow-sm opacity-100 tooltip">
+                                                    <div id="tooltip-light" role="tooltip" class="inline-block absolute visible py-2 px-1 text-sm font-medium text-gray-900 bg-white rounded-lg border border-gray-200 shadow-sm opacity-100 tooltip" style="z-index: 100;!important;">
                                                         <input class="focus:outline-none text-center text-yellow-500 text-xl" type="text" id="amount2" name="amount" readonly >
                                                         <div class="tooltip-arrow" data-popper-arrow></div>
                                                     </div>
