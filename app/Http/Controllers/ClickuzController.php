9e135d339334c386f6ab3a56af60a7fc12cb8d28
Abram73 <abram4ik73@gmail.com>
2022-03-01 20:45:17 +0500

    Modified

--- app/Http/Controllers/ClickuzController.php
+++ app/Http/Controllers/ClickuzController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace  App\Http\Controllers;
+namespace Teamprodev\Laravel_Payment_Clickuz\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Teamprodev\Laravel_Payment_Clickuz\Models\Complete;
