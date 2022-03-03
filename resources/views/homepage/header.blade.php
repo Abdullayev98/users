749d422fa34558dd762cce9d67ded7e2462278b7
asilbek20041207 <asilbek.pardabayev@bk.ru>
2022-03-03 14:22:57 +0500

    home page design

--- resources/views/homepage/header.blade.php
+++ resources/views/homepage/header.blade.php
@@ -1,39 +1,41 @@
 <link rel="stylesheet" href="{{ asset ('/css/header.css') }}">
 <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css"/>
-<div class="HomepageHeaderSection">
-    <div class="video-bg">
-        @if(setting('site.Video_bg') != null)
-            @php
-                $array_video = json_decode(setting('site.Video_bg'), true);
-                    $str_replace = str_replace("\\","/",$array_video);
-            @endphp
-            <video src="{{asset('storage/'.$array_video['0']['download_link'])}}" type="video/mp4" autoplay muted loop></video>
-        @else
-            @php
-                $pp = setting('site.foto_bg');
-                $str_replace = str_replace("\\","/",$pp);
-            @endphp
-            {{--            <img src="storage/{{$str_replace}}" alt="rasm yoq">--}}
-            <img src="{{ asset('/images/uborka1.jpg') }}" alt="rasm yoq">
-        @endif
-        <div class="effects"></div>
-        <div class="video-bg__content"></div>
-    </div>
-</div>
-<div class="relative z-999 top-0 left-50">
+
+
+{{--<div class="HomepageHeaderSection">--}}
+{{--    <div class="video-bg">--}}
+{{--        @if(setting('site.Video_bg') != null)--}}
+{{--            @php--}}
+{{--                $array_video = json_decode(setting('site.Video_bg'), true);--}}
+{{--                    $str_replace = str_replace("\\","/",$array_video);--}}
+{{--            @endphp--}}
+{{--            <video src="{{asset('storage/'.$array_video['0']['download_link'])}}" type="video/mp4" autoplay muted loop></video>--}}
+{{--        @else--}}
+{{--            @php--}}
+{{--                $pp = setting('site.foto_bg');--}}
+{{--                $str_replace = str_replace("\\","/",$pp);--}}
+{{--            @endphp--}}
+{{--            --}}{{--            <img src="storage/{{$str_replace}}" alt="rasm yoq">--}}
+{{--            <img src="{{ asset('/images/uborka1.jpg') }}" alt="rasm yoq">--}}
+{{--        @endif--}}
+{{--        <div class="effects"></div>--}}
+{{--        <div class="video-bg__content"></div>--}}
+{{--    </div>--}}
+{{--</div>--}}
+<div class="left-50" style="background: #FFF5E5; height: 500px;">
     <div class="w-xl">
-        <main class="md:w-7/12 w-10/12 mx-auto">
-            <div class="text-center pt-24">
-                <h1 class="font-bold text-white text-4xl lg:text-5xl">
+        <main class="w-10/12 mx-auto grid grid-cols-2">
+            <div class="pt-24 grid-cols-1">
+                <h1 class="font-bold text-4xl lg:text-5xl">
                     <span class="block">{{__('Освободим вас от забот')}}</span>
                 </h1>
-                <p class="mt-3 text-sm md:text-base text-white sm:mt-5 sm:mx-auto md:mt-3 md:md:mt-2 mb-3">
+                <p class="mt-3 text-sm md:text-base sm:mt-5 sm:mx-auto md:mt-3 md:md:mt-2 mb-3">
                     {{__('Поможем найти надежного исполнителя для любых задач')}}
                 </p>
                 <div class="mx-auto">
-                    <div class="lg:w-10/12 w-full mx-auto flex-1">
+                    <div class="lg:w-10/12 w-full flex-1 mt-8">
                         <input name="TypeList" list="TypeList" type="text" id="header_input" placeholder="{{__('Чем вам помочь...')}}"
-                               class="input_text w-full md:px-4 px-2 py-2.5 md:py-3 rounded-md focus:placeholder-transparent focus:outline-none focus:border-yellow-500 flex-1 md:text-xl text-lg">
+                               class="input_text w-full md:px-4 px-2 py-2.5 md:py-3 rounded-xl focus:placeholder-transparent focus:outline-none focus:border-yellow-500 flex-1 md:text-xl text-lg">
                         <datalist id="TypeList">
                             @foreach(\TCG\Voyager\Models\Category::query()->where('parent_id','!=',NULL)->get() as $category)
                                 <option
@@ -41,25 +43,45 @@
                             @endforeach
                         </datalist>
                         <a href="" type="submit" id="createhref"
-                           class="float-right sm:block hidden md:text-xl text-lg border bg-yellow-500 z-0 border-transparent rounded-md md:px-3.5 px-2 pt-2 pb-1.5 md:py-2.2 mr-1 md:mt-2 mt-2.5 -ml-24 md:-top-14 -top-14 relative text-white">
+                           class="float-right sm:block hidden md:text-xl text-lg border bg-blue-900 z-0 border-transparent rounded-xl md:px-3.5 px-2 pt-2 pb-1.5 md:py-2.2 mr-1 md:mt-2 mt-2.5 -ml-24 md:-top-14 -top-14 relative text-white">
                             {{__('Заказать услугу')}}
                         </a>
                         <a href="" type="submit" id="createhref"
-                           class="float-right sm:hidden block  md:text-xl  text-lg border bg-yellow-500 z-0 border-transparent rounded-md md:px-3.5 px-2 pt-2 pb-1.5 md:py-2 mr-1 md:mt-2 mt-2.5 -ml-24 md:-top-14 -top-14 relative text-white">
+                           class="float-right sm:hidden block  md:text-xl  text-lg border bg-blue-900 z-0 border-transparent rounded-xl md:px-3.5 px-2 pt-2 pb-1.5 md:py-2 mr-1 md:mt-2 mt-2.5 -ml-24 md:-top-14 -top-14 relative text-white">
                             Заказать
                         </a>
-                        <div class="text-left mt-2 text-gray-300 font-semibold underline-offset-1 text-xs">
-                            {{__('Например:')}}
-                            <a href="/task/create?category_id=22" id="span_demo" onclick="myFunction()" class="hover:text-slate-400  hover:text-gray-200 cursor-pointer">
-                                {{__('Услуги пешего курьера')}}
+{{--                        <div class="text-left mt-2 text-gray-300 font-semibold underline-offset-1 text-xs">--}}
+{{--                            {{__('Например:')}}--}}
+{{--                            <a href="/task/create?category_id=22" id="span_demo" onclick="myFunction()" class="hover:text-slate-400  hover:text-gray-200 cursor-pointer">--}}
+{{--                                {{__('Услуги пешего курьера')}}--}}
+{{--                            </a>--}}
+{{--                        </div>--}}
+                        <div class="mt-8">
+                            <a class="cursor-pointer">
+                                <i class="fab fa-instagram text-yellow-500 mx-2"></i>
+                            </a>
+                            <a class="cursor-pointer">
+                                <i class="fab fa-telegram text-yellow-500 mx-2"></i>
+                            </a>
+                            <a class="cursor-pointer">
+                                <i class="fab fa-youtube text-yellow-500 mx-2"></i>
+                            </a>
+                            <a class="cursor-pointer">
+                                <i class="fab fa-facebook text-yellow-500 mx-2"></i>
                             </a>
                         </div>
                     </div>
                 </div>
-                <div class="flex flex-row sm:w-1/2 w-5/6 mx-auto mt-8 items-center text-blue-300 hover:text-blue-400">
-                    <a href="{{route('verification')}}"><i class="text-blue fas fa-shield-alt text-2xl mx-2"></i></a>
-                    <a href="{{route('verification')}}"> <p class="text-base underline">
-                            {{__('стать исполнителем и начать зарабатывать')}}</p></a>
+{{--                <div class="flex flex-row sm:w-1/2 w-5/6 mx-auto mt-8 items-center text-blue-300 hover:text-blue-400">--}}
+{{--                    <a href="{{route('verification')}}"><i class="text-blue fas fa-shield-alt text-2xl mx-2"></i></a>--}}
+{{--                    <a href="{{route('verification')}}"> <p class="text-base underline">--}}
+{{--                            {{__('стать исполнителем и начать зарабатывать')}}</p></a>--}}
+{{--                </div>--}}
+            </div>
+            <div class="grid-cols-1">
+                <div>
+                    <img src="{{ asset('/images/bg_line.png') }}" alt="Line" class="absolute z-1 h-full" style=" height: 500px;">
+                    <img src="{{ asset('/images/Ellipse1.png') }}" alt="" class="relative z-10 top-6 ml-16" style=" height: 450px;">
                 </div>
             </div>
         </main>
--- resources/views/homepage/home_economy.blade.php
+++ resources/views/homepage/home_economy.blade.php
@@ -4,57 +4,119 @@
         <div class="text-4xl mx-auto py-10 md:py-16">
             {!!__('С Universal Services вы экономите на услугах до 70%.<br> Как это возможно')!!}
         </div>
-        <div class="grid md:grid-cols-2 grid-cols-1 mt-8 w-11/12 mx-auto">
+{{--        <div class="grid md:grid-cols-2 grid-cols-1 mt-8 w-11/12 mx-auto">--}}
 
-            @php $cnt_for_hiw = 0; @endphp
+{{--            @php $cnt_for_hiw = 0; @endphp--}}
 
-            @foreach($howitworks as $howitwork)
+{{--            @foreach($howitworks as $howitwork)--}}
 
-                @if(($cnt_for_hiw % 2) == 0)
+{{--                @if(($cnt_for_hiw % 2) == 0)--}}
 
-                    <div>
-                        <img class="lg:ml-0  mx-auto  w-42 h-42" src="{{ asset('storage/'.$howitwork->image) }}" alt="">
+{{--                   я--}}
+{{--                    <div class="md:text-left text-center">--}}
+{{--                        <h3 class="text-3xl my-8">{{ $howitwork->getTranslatedAttribute('title',Session::get('lang') , 'fallbackLocale') }}</h3>--}}
+{{--                        <a href="/categories/1" class="">--}}
+{{--                            <button class="bg-transparent hover:bg-yellow-500 text-yellow-500 hover:text-white py-2 px-4 border border-yellow-500 hover:border-transparent rounded">--}}
+{{--                                {{__('Создать задания')}}</button>--}}
+{{--                        </a>--}}
+{{--                    </div>--}}
+
+{{--                @else--}}
+
+{{--                    <div class="md:text-left text-center my-16 md:block hidden">--}}
+{{--                        <h1 class="text-3xl my-8">{{ $howitwork->getTranslatedAttribute('title',Session::get('lang') , 'fallbackLocale') }}</h1>--}}
+{{--                        <h2 class="text-xl">{{ strip_tags($howitwork->getTranslatedAttribute('description',Session::get('lang') , 'fallbackLocale')) }}</h2>--}}
+{{--                    </div>--}}
+{{--                    <div class="my-16 md:block hidden">--}}
+{{--                        <img class="lg:ml-0  mx-auto w-42 h-42" src="{{ asset('storage/'.$howitwork->image) }}" alt="">--}}
+{{--                    </div>--}}
+
+{{--                    <div class="my-16 md:hidden block">--}}
+{{--                        <img class="lg:ml-0 mx-auto w-42 h-42"--}}
+{{--                             src="{{ asset('storage/'.$howitwork->image) }}"--}}
+{{--                             alt="">--}}
+{{--                    </div>--}}
+{{--                    <div class="md:text-left text-center md:hidden block">и найдите <br> исполнителя за несколько минут--}}
+{{--                        <h3 class="text-3xl mt-8">{{ $howitwork->getTranslatedAttribute('title',Session::get('lang') , 'fallbackLocale') }}</h3>--}}
+{{--                        <a href="/categories/1" class="text-blue-500 underline text-xl">{{__('Создать задания')}}</a>--}}
+{{--                    </div>--}}
+{{--                @endif--}}
+{{--                @php $cnt_for_hiw++; @endphp--}}
+{{--            @endforeach--}}
+{{--        </div>--}}
+
+        <div class="grid md:grid-cols-3 grid-col-1 w-10/12 mx-auto">
+            <div class="grid-cols-1 text-left ">
+                <div class="border border-gray-300  mx-3 p-4 rounded-xl">
+                    <span class="font-semibold">
+                    <span class="text-yellow-500">
+                        1.
+                    </span> Создать задания
+                </span>
+                    <h1 class="my-2 pb-2">
+                        Опишите своими словами задачу, которую требуется выполнить Создать задание
+                    </h1>
+                    <a href="/categories/1"
+                       class="text-yellow-500 hover:text-white  p-3 rounded-lg transition duration-300 hover:bg-yellow-500">
+                        Создать задания
+                    </a>
+                </div>
+            </div>
+            <div class="grid-cols-1 text-left">
+                <div class="border border-gray-300  mx-3 p-4 rounded-xl">
+                    <span class="font-semibold">
+                    <span class="text-yellow-500">
+                        2.
+                    </span> Исполнители предложат вам свои услуги и цены
+                </span>
+                    <h1 class="my-2">
+                        Уже через пару минут вы начнете получать отклики от исполнителей, готовых выполнить ваше задание.
+                    </h1>
+                </div>
+            </div>
+            <div class="grid-cols-1 text-left">
+                <div class="border border-gray-300  mx-3 p-4 rounded-xl">
+                    <span class="font-semibold">
+                    <span class="text-yellow-500">
+                        3.
+                    </span> Выберите лучший отклик
+                </span>
+                    <h1 class="my-2">
+                        Вы сможете выбрать подходящего исполнителя, по разным критериям:
+                    </h1>
+                    <div class="flex">
+                        <i class="fas fa-thumbs-up text-yellow-400 mx-2 pr-3"></i>
+                        <p>Отзывы заказчиков</p>
                     </div>
-                    <div class="md:text-left text-center">
-                        <h3 class="text-3xl my-8">{{ $howitwork->getTranslatedAttribute('title',Session::get('lang') , 'fallbackLocale') }}</h3>
-                        <a href="/categories/1" class="">
-                            <button class="bg-transparent hover:bg-yellow-500 text-yellow-500 hover:text-white py-2 px-4 border border-yellow-500 hover:border-transparent rounded">
-                                {{__('Создать задания')}}</button>
-                        </a>
+                    <div class="flex">
+                        <i class="fas fa-user-alt text-yellow-400 mx-2 pr-3"></i>
+                        <p>Примеры работ</p>
                     </div>
+                    <div class="flex">
+                        <i class="fas fa-dollar-sign text-yellow-400 mx-2 pl-1 pr-4"></i>
+                        <p>Стоимость услуг</p>
+                    </div>
+                    <div class="flex">
+                        <i class="fas fa-star text-yellow-400 mx-2 pr-3"></i>
+                        <p>Рейтинг</p>
+                    </div>
+                </div>
+            </div>
 
-                @else
+        </div>
 
-                    <div class="md:text-left text-center my-16 md:block hidden">
-                        <h1 class="text-3xl my-8">{{ $howitwork->getTranslatedAttribute('title',Session::get('lang') , 'fallbackLocale') }}</h1>
-                        <h2 class="text-xl">{{ strip_tags($howitwork->getTranslatedAttribute('description',Session::get('lang') , 'fallbackLocale')) }}</h2>
-                    </div>
-                    <div class="my-16 md:block hidden">
-                        <img class="lg:ml-0  mx-auto w-42 h-42" src="{{ asset('storage/'.$howitwork->image) }}" alt="">
-                    </div>
-
-                    <div class="my-16 md:hidden block">
-                        <img class="lg:ml-0 mx-auto w-42 h-42"
-                             src="{{ asset('storage/'.$howitwork->image) }}"
-                             alt="">
-                    </div>
-                    <div class="md:text-left text-center md:hidden block">и найдите <br> исполнителя за несколько минут
-                        <h3 class="text-3xl mt-8">{{ $howitwork->getTranslatedAttribute('title',Session::get('lang') , 'fallbackLocale') }}</h3>
-                        <a href="/categories/1" class="text-blue-500 underline text-xl">{{__('Создать задания')}}</a>
-                    </div>
-                @endif
-                @php $cnt_for_hiw++; @endphp
-            @endforeach
+        <div class="w-8/12 mx-auto -mt-16">
+            <img src="{{ asset('/images/Vector.png') }}" alt="">
         </div>
     </div>
-    <div class="text-center w-full mx-auto mt-12 pb-8">
-        <a href="/task/create?category_id=31">
-            <button class="text-center text-white py-4 px-5  bg-yellow-500 border-yellow-500 text-3xl rounded">
-                {{__('Разместите задание прямо сейчас')}}
-            </button>
-        </a>
-        <div class="text-center text-xl mt-4">
-            {!!__('и найдите <br> исполнителя за несколько минут')!!}
-        </div>
-    </div>
+{{--    <div class="text-center w-full mx-auto mt-12 pb-8">--}}
+{{--        <a href="/task/create?category_id=31">--}}
+{{--            <button class="text-center text-white py-4 px-5  bg-yellow-500 border-yellow-500 text-3xl rounded">--}}
+{{--                {{__('Разместите задание прямо сейчас')}}--}}
+{{--            </button>--}}
+{{--        </a>--}}
+{{--        <div class="text-center text-xl mt-4">--}}
+{{--            {!!__('и найдите <br> исполнителя за несколько минут')!!}--}}
+{{--        </div>--}}
+{{--    </div>--}}
 </div>
