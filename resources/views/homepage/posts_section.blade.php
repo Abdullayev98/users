4b73f54eb3c0b2e54c78c3f5f2ec7f7ce1ca47db
Ozodbek1405 <roziyevozodbek75@gmail.com>
2022-03-03 15:33:48 +0500

    home page design

--- resources/views/homepage/posts_section.blade.php
+++ resources/views/homepage/posts_section.blade.php
@@ -1,5 +1,4 @@
 <div class="w-4/5 mx-auto my-16">
-<<<<<<< HEAD
     <div class="w-full">
         <h1 class="text-2xl font-bold">{{__('Что заказывают на «Универсал Сервис» прямо сейчас')}}</h1>
     </div>
@@ -18,37 +17,18 @@
                             <p class="text-base mt-2 overflow-hidden whitespace-nowrap text-ellipsis text-gray-400">
                                 {{$task->description}}
                             </p>
-=======
-        <div class="w-full">
-            <h1 class="text-2xl font-bold">{{__('Что заказывают на «Универсал Сервис» прямо сейчас')}}</h1>
-        </div>
-        <div class="grid md:grid-cols-3 grid-cols-2 mx-auto mb-56">
-            <div id="scrollbar" class="col-span-2 md:w-10/12 w-full h-screen blog1 mt-8">
-                <div class="w-full overflow-y-scroll h-screen border rounded-lg px-4 scrollbar" id="style-3">
-                    @foreach($tasks as $task)
-                        <div class="w-full border rounded-lg my-2 h-28 overflow-hidden force-overflow">
-                            <div class="icon pt-4">
-                                <i class="{{$task->category->ico}} text-3xl ml-4 float-left"></i>
-                            </div>
-                            <div class="mx-auto w-2/3">
-                                <a href="/detailed-tasks/{{$task->id}}" class="xl:text-2xl md:text-xl text-xl">
-                                    {{$task->name}}
-                                </a>
-                                <p class="text-base mt-2 overflow-hidden whitespace-nowrap text-ellipsis text-gray-400">
-                                    {{$task->description}}
-                                </p>
-                            </div>
                         </div>
-                    @endforeach
-                </div>
-                <div class="mt-8 flex justify-center">
-                    <a href="{{route('task.search')}}" type="button"
-                       class="text-center p-4 bg-blue-500 border-blue-500 text-white text-base  rounded-lg">
-                        {{__('Показать ещё задания')}}
-                    </a>
-
-                </div>
+                    </div>
+                @endforeach
             </div>
+            <div class="mt-8 flex justify-center">
+                <a href="{{route('task.search')}}" type="button"
+                   class="text-center p-4 bg-blue-500 border-blue-500 text-white text-base  rounded-lg">
+                    {{__('Показать ещё задания')}}
+                </a>
+
+            </div>
+        </div>
             <div class="w-full md:col-span-1  col-span-2">
                 <a href="{{route('verification')}}">
                     <div
@@ -57,7 +37,6 @@
                             <i class="fas fa-user text-green-300 text-5xl pt-8"></i>
                             <p class="lg:text-4xl xl:text-4xl md:text-2xl text-4xl font-medium text-white">
                                 {{__('Как стать исполнителем')}}</p>
->>>>>>> 749d422fa34558dd762cce9d67ded7e2462278b7
                         </div>
                     </div>
                 @endforeach
