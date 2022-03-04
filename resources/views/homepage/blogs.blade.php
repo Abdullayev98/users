63abbe8d4df4f62c38c1efa03cfed330afdab6e7
Komila <razikova09@mail.ru>
2022-03-04 15:16:43 +0500

    added box radius

--- resources/views/homepage/blogs.blade.php
+++ resources/views/homepage/blogs.blade.php
@@ -3,7 +3,7 @@
         <div class="w-1/3 sm:block hidden">
             <img src="{{asset('images/Rectangle1.png')}}" alt="#">
         </div>
-        <div class="sm:w-2/3 w-full p-3 shadow-md">
+        <div class="sm:rounded-r-2xl rounded-r-2xl-none sm:rounded-none rounded-2xl sm:w-2/3 w-full p-3 shadow-md">
             <h1 class="text-lg text-gray-700 font-medium">Удобная и безопасная оплата</h1>
             <p class="text-sm text-gray-600">При оплате через Сделку без риска Universal Services вернет деньги, если что-то пойдет   не так.</p>
         </div>
@@ -12,7 +12,7 @@
         <div class="w-1/3 sm:block hidden">
             <img src="{{asset('images/Rectangle2.png')}}" alt="#">
         </div>
-        <div class="sm:w-2/3 w-full p-3 shadow-md">
+        <div class="sm:rounded-r-2xl rounded-r-2xl-none sm:rounded-none rounded-2xl sm:w-2/3 w-full p-3 shadow-md">
             <h1 class="text-lg text-gray-700 font-medium">Надежные исполнители</h1>
             <p class="text-sm text-gray-600">«Проверенные исполнители» подтвердили свои документы на Universal Services.</p>
         </div>
@@ -21,7 +21,7 @@
         <div class="w-1/3 sm:block hidden">
             <img src="{{asset('images/Rectangle3.png')}}" alt="#">
         </div>
-        <div class="sm:w-2/3 w-full p-3 shadow-md">
+        <div class="sm:rounded-r-2xl rounded-r-2xl-none sm:rounded-none rounded-2xl sm:w-2/3 w-full p-3 shadow-md">
             <h1 class="text-lg text-gray-700 font-medium">Достоверные отзывы</h1>
             <p class="text-sm text-gray-600">Более 1 000 000 отзывов от заказчиков помогут выбрать подходящего исполнителя.</p>
         </div>
@@ -30,7 +30,7 @@
         <div class="w-1/3 sm:block hidden">
             <img src="{{asset('images/Rectangle4.png')}}" alt="#">
         </div>
-        <div class="sm:w-2/3 w-full p-3 shadow-md">
+        <div class="sm:rounded-r-2xl rounded-r-2xl-none sm:rounded-none rounded-2xl sm:w-2/3 w-full p-3 shadow-md">
             <h1 class="text-lg text-gray-700 font-medium">Universal Services для бизнеса</h1>
             <p class="text-sm text-gray-600">Безналичная оплата бизнес-заданий с предоставлением закрывающих документов.</p>
         </div>
