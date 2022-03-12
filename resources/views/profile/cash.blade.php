22bff142256693d49ce3970cd84985ed8dd77e89
Ozodbek1405 <roziyevozodbek75@gmail.com>
2022-03-12 17:35:15 +0500

    bug fix profile blade

--- resources/views/profile/cash.blade.php
+++ resources/views/profile/cash.blade.php
@@ -9,19 +9,18 @@
                 @include('components.profileFigure')
 
                 <div class="content  mt-20 ">
-                    <div class="grid md:grid-cols-10 w-full">
+                    <div class="grid md:grid-cols-10 w-full ">
                         <ul class="md:col-span-9 col-span-10 items-center w-3/4 md:w-full">
                             <li class="inline mr-1 md:mr-5"><a href="/profile"
-                                    class="md:text-[18px] text-[14px] text-gray-600">{{__('Обо мне')}}</a></li>
+                                    class="text-lg text-gray-600">{{__('Обо мне')}}</a></li>
                             <li class="inline mr-1 md:mr-5"><a href="/profile/cash"
-                                    class="md:text-[18px] text-[14px] font-bold text-gray-700">{{__('Счет')}}</a>
+                                    class="text-lg font-bold text-gray-700 border-b-4 border-green-500 pb-1">{{__('Счет')}}</a>
                             </li>
                             <li class=" md:mr-5 mr-1 inline-block md:hidden block"><a href="/profile/settings"
-                                    class="md:text-[18px] text-[14px] text-gray-600"
+                                    class="text-lg text-gray-600"
                                     id="settingsText">{{__('Настройки')}}</a></li>
                         </ul>
-                        <div class="md:col-span-1 md:block hidden" id="settingsIcon"><a href="/profile/settings"><i
-                                    class="fas fa-user-cog text-3xl text-gray-600"></i></a></div>
+                        <div class="md:col-span-1 md:block hidden ml-4" id="settingsIcon"><a href="/profile/settings"><i class="fas fa-cog text-2xl"></i></a></div>
                     </div>
                     <hr>
 
@@ -180,30 +179,86 @@
                         </div>
                     </div>
                 </div>
+                    @if(auth()->user()->role_id!=2)
+                        <a href="/verification" class="flex flex-row shadow-lg rounded-lg mb-8">
+                            <div class="w-1/2 h-24 bg-contain bg-no-repeat bg-center" style="background-image: url({{asset('images/like.png')}});">
+                            </div>
+                            <div class="font-bold text-xs text-gray-700 text-left my-auto">
+                            {{__('Станьте исполнителем')}}<br>{{__('U-ser. И начните')}}<br> {{__('зарабатывать.')}}        </div>
+                        </a>
+                    @endif
+                <div class="mt-6 ml-4">
+                    @if (auth()->user()->role_id==2)
+                        <h3 class="font-medium text-gray-700 text-3xl">
+                            {{__('Исполнитель')}}
+                        </h3>
+                    @endif
+                    <p>
+                        {{__('на Universal Services с 12 сентября 2021 г.')}}
+                    </p>
+                </div>
+                <div class="contacts">
+                    @if($user->google_id)
+            
+                        <div class="telefon ml-4 h-20 grid grid-cols-4">
+                            <div class="w-12 h-12 text-center mx-auto my-auto py-2 bg-gray-300 rounded-xl col-span-1"
+                                 style="background-color: #4285F4;">
+                                <i class="fab fa-google text-white"></i>
+                            </div>
+                            <div class="ml-3 col-span-3">
+                                <h5 class="font-bold text-gray-700 block mt-2 text-md">Google</h5>
+                                {{__('Подтвержден')}}
+                            </div>
+                        </div>
+                    @endif
+                    @if($user->facebook_id)
+                        <div class="telefon ml-4 h-20 grid grid-cols-4">
+                            <div class="w-12 h-12 text-center mx-auto my-auto py-2 bg-gray-300 rounded-xl col-span-1"
+                                 style="background-color: #4285F4;">
+                                <i class="fab fa-facebook-f text-white"></i>
+                            </div>
+                            <div class="ml-3 col-span-3">
+                                <h5 class="font-bold text-gray-700 block mt-2 text-md">Facebook</h5>
+                                <p>{{__('Подтвержден')}}</p>
+                            </div>
+                        </div>
+                    @endif
+                </div>
                 <p class="mx-5 my-4">
-                    {{__('Повысьте доверие пользователей к себе — привяжите ваши аккаунты социальных сетей к профилю Servicebox. Мы обязуемся не раскрывать ваши контакты.')}}</p>
-                <div class="telefon ml-4 h-20 grid grid-cols-4 content-center">
-                    <div class="w-12 h-12 text-center mx-auto my-auto py-2 bg-gray-300 rounded-xl col-span-1"
-                        style="background-color: #4285F4;">
-                        <i class="fab fa-google text-white text-2xl"></i>
+                    {{__('Повысьте доверие пользователей к себе — привяжите ваши аккаунты социальных сетей к профилю Servicebox. Мы обязуемся не раскрывать ваши контакты.')}}
+                </p>
+                @if(!$user->google_id)
+            
+                    <div class="telefon ml-4 h-20 grid grid-cols-4">
+                        <div class="w-12 h-12 text-center mx-auto my-auto py-2 bg-gray-300 rounded-xl col-span-1"
+                             style="background-color: #4285F4;">
+                            <i class="fab fa-google text-white text-2xl"></i>
+                        </div>
+                        <div class="ml-3 col-span-3">
+                            <h5 class="font-bold text-gray-700 block mt-4 text-md">Google</h5>
+                            <a href="{{route('auth.google')}}" target="_blank"
+                               class="block text-sm">
+                            {{__('Привязать')}}
+                            </a></p></a>
+                        </div>
                     </div>
-                    <div class="ml-3 col-span-3">
-                        <h5 class="font-bold text-gray-700 block text-md">Google</h5>
-                        <a href="https://www.google.com/" target="_blank"
-                            class="block text-sm">{{__('Привязать')}}</a></p></a>
+                @endif
+                @if(!$user->facebook_id)
+                    <div class="telefon ml-4 h-20 grid grid-cols-4">
+                        <div class="w-12 h-12 text-center mx-auto my-auto py-2 bg-gray-300 rounded-xl col-span-1"
+                             style="background-color: #4285F4;">
+                            <i class="fab fa-facebook-f text-white text-2xl"></i>
+                        </div>
+                        <div class="ml-3 col-span-3">
+                            <h5 class="font-bold text-gray-700 block mt-4 text-md">Facebook</h5>
+                            <a href="{{route('auth.facebook')}}" target="_blank"
+                               class="block text-sm">
+                            {{__('Привязать')}}
+                            </a>
+                        </div>
                     </div>
-                </div>
-                <div class="telefon ml-4 h-20 grid grid-cols-4 content-center">
-                    <div class="w-12 h-12 text-center mx-auto my-auto py-2 bg-gray-300 rounded-xl col-span-1"
-                        style="background-color: #4285F4;">
-                        <i class="fab fa-facebook-f text-white text-2xl"></i>
-                    </div>
-                    <div class="ml-3 col-span-3">
-                        <h5 class="font-bold text-gray-700 block text-md">Facebook</h5>
-                        <a href="https://www.facebook.com/" target="_blank"
-                            class="block text-sm">{{__('Привязать')}}</a>
-                    </div>
-                </div>
+                @endif
+            
             </div>
             {{-- tugashi o'ng tomon ispolnitel --}}
         </div>
