5ea4f20f06af2bc60ca7dad0ada82f9ff7c0db26
Shuxrat78 <gl-ob_asr@mail.ru>
2022-03-17 09:45:20 +0500

    Compliance va CompliancesType Model lari qo'shildi...

--- resources/views/task/changetask.blade.php
+++ resources/views/task/changetask.blade.php
@@ -148,7 +148,7 @@
                     <div class="mb-4">
                         <div id="formulario" class="flex flex-col gap-y-4 text-base">
 
-                            <div class="flex items-center bg-white hover:bg-gray-100  rounded-lg border py-1">
+                            <div class="flex items-center bg-white hover:bg-gray-100 rounded-lg border py-1 mb-2">
                                 <button
                                     class="flex-shrink-0 border-transparent text-teal-500 text-md py-1 px-2 rounded focus:outline-none"
                                     type="button">
@@ -183,84 +183,17 @@
 
                             <input name="coordinates0" type="hidden" id="coordinate"
                                    value="{{json_decode($task->address)->latitude.",". json_decode($task->address)->longitude }}">
-{{--                            <div id="addinput" class="flex gap-y-2 flex-col">--}}
-{{--                                                                @foreach(json_decode($task->address_add) as $locate)--}}
-{{--                                    <div class="flex items-center gap-x-2">--}}
-{{--                                        <div class="flex items-center rounded-lg border  w-full py-1">--}}
-{{--                                            <button--}}
-{{--                                                class="flex-shrink-0 border-transparent text-teal-500 text-md py-1 px-2 rounded focus:outline-none"--}}
-{{--                                                type="button">--}}
-{{--                                                <script>alp[{{$loop->index+1}}]</script>--}}
-{{--                                            </button>--}}
-{{--                                            <input oninput="myFunction()" id="suggest{{$loop->index+1}}"--}}
-{{--                                                   class="appearance-none bg-transparent w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none"--}}
-{{--                                                   type="search" name="location{{$loop->index+1}}"--}}
-{{--                                                   placeholder="Город, Улица, Дом" aria-label="Full name"--}}
-{{--                                                   value="{{ $locate->location }}">--}}
-{{--                                            <button id="{{$loop->index+1}}" onclick="getLocals(this.id)"--}}
-{{--                                                    class="flex-shrink-0 border-transparent border-4 text-teal-500 hover:text-teal-800 text-sm py-1 px-2 rounded"--}}
-{{--                                                    type="button">--}}
-{{--                                                <svg className="h-4 w-4 text-purple-500" width="18" height="18"--}}
-{{--                                                     viewBox="0 0 24 24" stroke-width="2" stroke="currentColor"--}}
-{{--                                                     fill="none" stroke-linecap="round" stroke-linejoin="round">--}}
-{{--                                                    <path stroke="none" d="M0 0h24v24H0z"/>--}}
-{{--                                                    <path--}}
-{{--                                                        d="M21 3L14.5 21a.55 .55 0 0 1 -1 0L10 14L3 10.5a.55 .55 0 0 1 0 -1L21 3"/>--}}
-{{--                                                </svg>--}}
-{{--                                            </button>--}}
-{{--                                        </div>--}}
-{{--                                        <button id="remove_inputs"--}}
-{{--                                                class="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow">--}}
-{{--                                            <svg xmlns="http://www.w3.org/2000/svg" width="18" height="20" fill="none">--}}
-{{--                                                <path fill-rule="evenodd" clip-rule="evenodd"--}}
-{{--                                                      d="M3.25 2.95v-.2A2.75 2.75 0 0 1 6 0h6a2.75 2.75 0 0 1 2.75 2.75v.2h2.45a.8.8 0 0 1 0 1.6H.8a.8.8 0 1 1 0-1.6h2.45zm10 .05v-.25c0-.69-.56-1.25-1.25-1.25H6c-.69 0-1.25.56-1.25 1.25V3h8.5z"--}}
-{{--                                                      fill="#666"/>--}}
-{{--                                                <path--}}
-{{--                                                    d="M14.704 6.72a.8.8 0 1 1 1.592.16l-.996 9.915a2.799 2.799 0 0 1-2.8 2.802h-7c-1.55 0-2.8-1.252-2.796-2.723l-1-9.994a.8.8 0 1 1 1.592-.16L4.3 16.794c0 .668.534 1.203 1.2 1.203h7c.665 0 1.2-.536 1.204-1.282l1-9.995z"--}}
-{{--                                                    fill="#666"/>--}}
-{{--                                                <path--}}
-{{--                                                    d="M12.344 7.178a.75.75 0 1 0-1.494-.13l-.784 8.965a.75.75 0 0 0 1.494.13l.784-8.965zm-6.779 0a.75.75 0 0 1 1.495-.13l.784 8.965a.75.75 0 0 1-1.494.13l-.785-8.965z"--}}
-{{--                                                    fill="#666"/>--}}
-{{--                                            </svg>--}}
-{{--                                        </button>--}}
-{{--                                        <input name="coordinates{{$loop->index+1}}" type="hidden"--}}
-{{--                                               id="coordinate{{$loop->index+1}}">--}}
-{{--                                    </div>--}}
-{{--                                    <script>x++--}}
-{{--                                        console.log(x)</script>--}}
-{{--                                @endforeach--}}
-{{--                            </div>--}}
+
                         </div>
                         <div>
 
                             <div class="mb-4">
                                 <div id="formulario" class="flex flex-col gap-y-4">
 
-                                    <div id="addinput" class="flex gap-y-2 flex-col bg-white hover:bg-gray-100 ">
-
-{{--                                        @php $loc_ch = array('B','C','D','E','F','G','H','I','J');--}}
-{{--                                        @endphp--}}
-{{--                                        @foreach(json_decode($task->address_add) as $locate)--}}
-{{--                                            <div class="flex items-center gap-x-2">--}}
-{{--                                                <div class="flex items-center rounded-lg border  w-full py-1">--}}
-{{--                                                    <button class="flex-shrink-0 border-transparent text-teal-500 text-md py-1 px-2 rounded focus:outline-none" type="button">{{$loc_ch[$loop->index]}}</button>--}}
-{{--                                                    <input oninput="myFunction()" id="suggest{{$loop->index+1}}" class="appearance-none bg-transparent w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none"--}}
-{{--                                                           type="search" name="location{{$loop->index+1}}" placeholder="Город, Улица, Дом" aria-label="Full name"--}}
-{{--                                                           value="{{ $locate->location }}">--}}
-{{--                                                    <button id="{{$loop->index+1}}" onclick="getLocals(this.id)" class="flex-shrink-0 border-transparent border-4 text-teal-500 hover:text-teal-800 text-sm py-1 px-2 rounded" type="button">--}}
-{{--                                                        <svg className="h-4 w-4 text-purple-500" width="18" height="18" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">--}}
-{{--                                                            <path stroke="none" d="M0 0h24v24H0z"/><path d="M21 3L14.5 21a.55 .55 0 0 1 -1 0L10 14L3 10.5a.55 .55 0 0 1 0 -1L21 3"/></svg>--}}
-{{--                                                    </button>--}}
-{{--                                                </div>--}}
-{{--                                                <button id="remove_inputs" class="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow">--}}
-{{--                                                    <svg xmlns="http://www.w3.org/2000/svg" width="18" height="20" fill="none"><path fill-rule="evenodd" clip-rule="evenodd" d="M3.25 2.95v-.2A2.75 2.75 0 0 1 6 0h6a2.75 2.75 0 0 1 2.75 2.75v.2h2.45a.8.8 0 0 1 0 1.6H.8a.8.8 0 1 1 0-1.6h2.45zm10 .05v-.25c0-.69-.56-1.25-1.25-1.25H6c-.69 0-1.25.56-1.25 1.25V3h8.5z" fill="#666"/>--}}
-{{--                                                        <path d="M14.704 6.72a.8.8 0 1 1 1.592.16l-.996 9.915a2.799 2.799 0 0 1-2.8 2.802h-7c-1.55 0-2.8-1.252-2.796-2.723l-1-9.994a.8.8 0 1 1 1.592-.16L4.3 16.794c0 .668.534 1.203 1.2 1.203h7c.665 0 1.2-.536 1.204-1.282l1-9.995z" fill="#666"/>--}}
-{{--                                                        <path d="M12.344 7.178a.75.75 0 1 0-1.494-.13l-.784 8.965a.75.75 0 0 0 1.494.13l.784-8.965zm-6.779 0a.75.75 0 0 1 1.495-.13l.784 8.965a.75.75 0 0 1-1.494.13l-.785-8.965z" fill="#666"/></svg> </button>--}}
-{{--                                                <input name="coordinates{{$loop->index+1}}" type="hidden" id="coordinate{{$loop->index+1}}">--}}
-{{--                                            </div>--}}
-{{--                                        @endforeach--}}
+                                    <div id="addinput" class="flex gap-y-2 flex-col hover:bg-gray-100 ">
 
                                     </div>
+
                                 </div>
                                 <div class="mt-4">
                                     <button id="addbtn" type="button"
