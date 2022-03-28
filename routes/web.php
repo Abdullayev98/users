c0aa91ac6070afb61bdec8d50545e60c86ae3e5e
Avazbek Nematov <68123420+avacoderuz@users.noreply.github.com>
2022-03-29 00:07:40 +0500

    all routes in one part 2

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\API\PerformerAPIController;
 use App\Http\Controllers\Controller;
 use App\Http\Controllers\FaqsController;
+use App\Http\Controllers\LoginController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\Task\ResponseController;
 use App\Http\Controllers\Task\UpdateController;
@@ -230,3 +231,36 @@ Route::post('send-review-user/{task}', [UpdateController::class, 'sendReview'])-
 
 Route::get('/faq',[FaqsController::class, 'index'])->name('faq.index');
 Route::get('/questions/{id}', [FaqsController::class,'questions'])->name('questions');
+
+
+Route::get('/categories/{id}', [Controller::class, 'category'])->name("categories");
+Route::get('/lang/{lang}', [Controller::class, 'lang'])->name('lang');
+
+
+
+Route::get('/login', [LoginController::class, 'login'])->name('login')->middleware('guest');
+Route::post('/login', [LoginController::class, 'loginPost'])->name('signin.custom')->middleware('guest');
+
+
+Route::get('/register', [UserController::class, 'signup'])->name('register')->middleware('guest');
+Route::post('/register', [LoginController::class, 'customRegister'])->name('user.registration')->middleware('guest');
+
+Route::get('/logout', [LoginController::class, 'logout'])->name('logout');
+
+Route::get('/reset', [UserController::class, 'reset'])->name('reset');
+
+Route::get('/confirm', [UserController::class, 'confirm'])->name('confirm');
+
+//Route::get('dashboard', [UserController::class, 'dashboardView'])->middleware(['auth', 'is_verify_email']);
+
+
+
+Route::get('dashboard', [UserController::class, 'dashboardView'])->middleware(['auth']);
+Route::get('account/verify/{user}/{hash}', [LoginController::class, 'verifyAccount'])->name('user.verify');
+Route::get('account/verification/email', [LoginController::class, 'send_email_verification'])->name('user.verify.send')->middleware('auth');
+Route::get('account/verification/phone', [LoginController::class, 'send_phone_verification'])->name('user.verify.phone.send')->middleware('auth');
+Route::post('account/verification/phone', [LoginController::class, 'verify_phone'])->name('user.verify.phone.submit')->middleware('auth');
+Route::post("account/change/email", [LoginController::class,'change_email'])->name('user.email.change')->middleware('auth');
+Route::post("account/change/phone", [LoginController::class,'change_phone_number'])->name('user.phone.change')->middleware('auth');
+Route::post("account/change/phone/send", [LoginController::class,'verify_phone'])->name('user.phone.verify')->middleware('auth');
+
