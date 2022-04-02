43e652bd36c274f7f3d16915536a98625791319f
james-msr <smr01234585@gmail.com>
2022-04-02 16:29:18 +0500

    Add clickuz tranzaction history

--- public/js/profile/cash.js
+++ public/js/profile/cash.js
@@ -22,26 +22,26 @@ function inputCash() {
         x.classList.add("hidden");
     }
     // tab content
-     let tabsContainer = document.querySelector("#tabs");
-     let tabTogglers = tabsContainer.querySelectorAll("#tabs a");
-     console.log(tabTogglers);
-     tabTogglers.forEach(function(toggler) {
-         toggler.addEventListener("click", function(e) {
-             e.preventDefault();
-             let tabName = this.getAttribute("href");
-             let tabContents = document.querySelector("#tab-contents");
-             for (let i = 0; i < tabContents.children.length; i++) {
-                 tabTogglers[i].parentElement.classList.remove("bg-gray-400", "rounded-sm",
-                     "text-white");
-                 tabContents.children[i].classList.remove("hidden");
-                 if ("#" + tabContents.children[i].id === tabName) {
-                     continue;
-                 }
-                 tabContents.children[i].classList.add("hidden");
-             }
-             e.target.parentElement.classList.add("bg-gray-400", "rounded-sm", "text-white");
-         });
-     });
+    //  let tabsContainer = document.querySelector("#tabs");
+    //  let tabTogglers = tabsContainer.querySelectorAll("#tabs a");
+    //  console.log(tabTogglers);
+    //  tabTogglers.forEach(function(toggler) {
+    //      toggler.addEventListener("click", function(e) {
+    //          e.preventDefault();
+    //          let tabName = this.getAttribute("href");
+    //          let tabContents = document.querySelector("#tab-contents");
+    //          for (let i = 0; i < tabContents.children.length; i++) {
+    //              tabTogglers[i].parentElement.classList.remove("bg-gray-400", "rounded-sm",
+    //                  "text-white");
+    //              tabContents.children[i].classList.remove("hidden");
+    //              if ("#" + tabContents.children[i].id === tabName) {
+    //                  continue;
+    //              }
+    //              tabContents.children[i].classList.add("hidden");
+    //          }
+    //          e.target.parentElement.classList.add("bg-gray-400", "rounded-sm", "text-white");
+    //      });
+    //  });
 
 // input cash transaction
 $('#period').change(function(){
